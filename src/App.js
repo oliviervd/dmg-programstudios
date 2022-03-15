2e6c03f6c7c1ed5c3d40457296682c848d4cddb4
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-15 20:28:07 +0100

    create function that takes random image and loads via IIIF image API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React, {Component} from "react"
+import React, {Component} from "react";
+import ReactDOM from "react-dom"
+import object_colors from "./data/objects_color.json";
 
 class App extends Component {
     render() {
+
+        var click = 0;
+        const handleClick = () => {
+            click += 1
+            // convert string to array and clean up value
+            console.log(object_colors[click]["IIIF_image"].replace("['","").replace("']","").split(","))
+            var _im =  object_colors[click]["IIIF_image"].replace("['","").replace("']","").split(",")
+            console.log(_im[0])
+
+            ReactDOM.render(
+                <img src={_im[0]} className="gridImage"></img>,
+              document.getElementById("imageRandom")
+            );
+        }
+
         return (
-            <h1>SPECTRUM</h1>
+            <body>
+                <h1>SPECTRUM</h1>
+                <h2>a DMG_ experiment</h2>
+                <button onClick={handleClick}>random object</button>
+                <div className={"container"}>
+                    <div id="imageRandom"></div>
+                </div>
+            </body>
         )
-    }
+    };
 }
 
 export default App;
