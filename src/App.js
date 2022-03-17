51794157cdcce1c7aa8ddb799ad80a7e8b9a21c4
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-17 17:23:26 +0100

    clean App, move functions and classes to components.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import React, {Component} from "react";
-import ReactDOM from "react-dom"
+import React, {Component, useState} from "react";
+import masonryGrid from "./components/masonry.py";
+import ColorPicker from "./components/colorPicker";
 import object_colors from "./data/objects_color.json";
-
-
-object_colors = object_colors.filter(function(entry) {
-    return entry.color_names.includes("black")
-});
 
 class App extends Component {
     render() {
 
-        const handleClick = () => {
-
-            var images = '';
-
-            for (var i=1; i<25; ++i) {
-                // pick random image from the collection
-                var x = Math.floor(Math.random() * object_colors.length) // todo: add function that uses each number only once.
-                // convert string to array and clean up value
-                var _im = object_colors[x]["IIIF_image"].replace("['","").replace("']","").replace("'","").split(",")
-                console.log(_im[0])
-                images += '<img src='+_im[0].replace("/full/0/default.jpg","/500,/0/default.jpg")+'></img>';
-            }
-            document.getElementById('imageRandom').innerHTML = images
-        }
-
         return (
             <div>
-                <h1>SPECTRUM</h1>
-                <h2>a DMG_ experiment</h2>
-                <button onClick={handleClick}>random object</button>
-                <div id="imageRandom" className={"container"}></div>
+                <div>
+                    <h1 onClick={masonryGrid}>SPECTRUM</h1>
+                    <ColorPicker/>
+                    <h4>pull. transmit. fall in love, and break up again.</h4>
+                    <div className={"container"} id={"imageRandom"}>
+                    </div>
+                </div>
             </div>
         )
     };
