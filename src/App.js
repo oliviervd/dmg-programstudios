5d86e6e9079a05d629a30c01f52a988e743fb56a
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-18 22:29:10 +0100

    rework structure. Pure out App.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React, {Component, useState} from "react";
-import masonryGrid from "./components/masonry.py";
+import React, {useState} from "react";
+import MasonryGrid from "./components/masonry.py";
 import ColorPicker from "./components/colorPicker";
-import object_colors from "./data/objects_color.json";
 
-class App extends Component {
-    render() {
+/*
+STRUCTURE:
 
-        return (
+>- app
+>-- color picker
+>-- catalogue
+>---- image
+>---- color hex
+>---- info text
+
+ */
+
+function App() {
+
+    return (
+        <div>
             <div>
-                <div>
-                    <h1 onClick={masonryGrid}>SPECTRUM</h1>
-                    <ColorPicker/>
-                    <h4>pull. transmit. fall in love, and break up again.</h4>
-                    <div className={"container"} id={"imageRandom"}>
-                    </div>
+                <h1>SPECTRUM</h1>
+                <ColorPicker></ColorPicker>
+                <h4>pull. transmit. fall in love, and break up again.</h4>
+                <button onClick = {MasonryGrid}>GENERATE</button>
+                <div className={"container"} id={"imageRandom"}>
                 </div>
             </div>
-        )
-    };
+        </div>
+    );
 }
+
 
 export default App;
