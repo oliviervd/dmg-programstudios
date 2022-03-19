33d7a9c70f8eb2c55b747238f89bfa077a6de70c
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-19 12:14:04 +0100

    META HEADER FLUXING AS A CHAMP.

---
+++ src/components/xeno.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const XenoHeader = () => {
+    return (
+        <div>
+            <div className="scroll-container">
+                <h1 className="scroll-text"> models from the past for the future | </h1>
+                <h1 className="scroll-text"> models from the past for the future | </h1>
+                <h1 className="scroll-text"> models from the past for the future | </h1>
+                <h1 className="scroll-text"> models from the past for the future | </h1>
+            </div>
+            <div className="scroll-container">
+                <h1 className="scroll-text">SPECTRUM | the transhistorical | </h1>
+                <h1 className="scroll-text">SPECTRUM | the current | </h1>
+                <h1 className="scroll-text">SPECTRUM | the xeno-morphic | </h1>
+                <h1 className="scroll-text">SPECTRUM | the past-future | </h1>
+            </div>
+        </div>
+    )
+}
+
+export default XenoHeader;
