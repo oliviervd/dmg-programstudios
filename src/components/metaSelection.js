01256c3a9feb8222dac62426854018f6a867bdb1
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-19 12:13:51 +0100

    some meta shit.

---
+++ src/components/metaSelection.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MetaSelection = (props) => {
+
+    return(
+        <div>
+            <p>selected color : {props.col}</p>
+        </div>
+    )
+}
+
+export default MetaSelection;
