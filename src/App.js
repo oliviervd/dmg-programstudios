86efcef57d48e308f34ac096053ff8876dd5cb32
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 20:01:25 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {useState} from "react";
 import XenoHeader from "./components/xeno"
 import SpectreMain from "./components/spectre"
+import ProjectDescription from "./components/projectDescription";
 import {spectrumProvider} from "./spectrumContext";
+import $ from 'jquery';
 
 function App() {
+
+    const _objectNum = 4;
 
     const [colors, setColors] = useState({
         color: ""
@@ -17,12 +21,40 @@ function App() {
         console.log(colors)
     }
 
+    var h = $('#handle'),
+        l = $('#left'),
+        r = $('#right'),
+        w = $('body').width() - 18;
+
+    var isDragging = false;
+
+    h.mousedown(function(e){
+        isDragging = true;
+        e.preventDefault();
+    });
+    $(document).mouseup(function(){
+        isDragging = false;
+    }).mousemove(function(e){
+        if(isDragging){
+            l.css('width', e.pageX);
+            r.css('width', w - e.pageX);
+        }
+    });
+
     return (
         <div>
             <spectrumProvider>
                 <XenoHeader/>
-                <SpectreMain/>
-                {/*<form onSubmit={handleSubmit}>
+                <div className="mainContainer">
+                    <ProjectDescription></ProjectDescription>
+                    <SpectreMain num={_objectNum}/>
+                </div>
+
+
+
+
+
+                    {/*<form onSubmit={handleSubmit}>
                     a spectrum of <br/>
                     <input onChange={(c) => setColors({...colors, color: c.target.value})}
                            type="text"
@@ -30,7 +62,6 @@ function App() {
                     traversing an ocean of desire
                 </form>
                 <MetaSelection col={colors.color}/>*/}
-
             </spectrumProvider>
         </div>
     );
