8246862b106d9c842e6333b3531f0e28256031ff
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 11:58:33 +0100

    add function "numSelect" that generates unique numbers to ensure that objects aren't shown twice in the masonry.

--- src/components/masonry.js
+++ src/components/masonry.js
@@ -1,30 +1,48 @@
+import {useState} from 'react';
 import object_colors from "../data/objects_color.json";
 
-const MasonryGrid = (props) => {
+//todo: move both object_colors and nanNums to app.js
 
+object_colors = object_colors.filter(function(entry) {
+    return entry.color_names.includes("coffee") //filter objects based on picked color.
+})
+
+function numSelect(i) {
+
+    // this function creates an array of unique numbers based on the length of object_colors.
+    var nums = [];
+    var ranNums = [];
+    var j = 0;
+    for (var n=0; n<object_colors.length; ++n) {
+        var x = Math.floor(Math.random() * object_colors.length)
+        nums.push(n);
+    }
+    while (i--) {
+        j = Math.floor(Math.random() * nums.length);
+        ranNums.push(nums[j]);
+        nums.splice(j,1);
+    }
+    return ranNums
+}
+
+const MasonryGrid = () => {
+
+        var _xList = numSelect(5);
         var images = '';
+        //console.log(props.c);
 
-        object_colors = object_colors.filter(function(entry) {
-            return entry.color_names.includes("green") //todo:fix
-        })
-
-        console.log(props.c);
-
-        for (var i=1; i<6; ++i) {
-            // pick random image from the collection
-            var x = Math.floor(Math.random() * object_colors.length) // todo: add function that uses each number only once.
-            // convert string to array and clean up value
+        for (var i=0; i<5; ++i) {
+            var x = _xList[i];
             var _im = object_colors[x]["IIIF_image"].replace("['","").replace("']","").replace("'","").split(",")
+            var _hexV = object_colors[x]["HEX_values"].split(",");
 
             images += '<div>' +
-                '<img src='+_im[0].replace("/full/0/default.jpg","/500,/0/default.jpg")+'></img>' +
-                '<div class="hexBox"> In 1967 "Marine" was loved by [author] when he used this color in the process of crafting this object.' +
-                '</div>' +
+                '<img alt="/src/media/alt_img.png" src='+_im[0].replace("/full/0/default.jpg","/500,/0/default.jpg")+'></img>' +
+                '<p>'+{_hexV}+'</p>'+
                 '</div>';
-
         }
-
         document.getElementById('imageRandom').innerHTML = images
+        console.log(_xList)
     };
 
 export default MasonryGrid;
