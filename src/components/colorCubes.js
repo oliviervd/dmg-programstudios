be0eb0c5de4a9e8fe1d38c59f122d23ad99b4582
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 22:03:21 +0100

    set max length for strings.

--- src/components/colorCubes.js
+++ src/components/colorCubes.js
@@ -5,21 +5,18 @@ import HexCube from "../components/cube"
 
 const ColorCubes = (props) => {
 
+        const maxL = 198;
+
         const selection = props.curation
         const color_hex = [];
+        const color_hex_text = [];
         for (var hexNum=0; hexNum<4; ++hexNum) {
             const x = selection[hexNum];
             var _hexVal = object_colors[x]["HEX_values"].replace("['","").replace("']","").replace("'","").split(",")
+            const _hexValStr = object_colors[x]["HEX_values"].replace("['","").replace("']","").replace("'","").split("[")
             color_hex.push(_hexVal)
+            color_hex_text.push(_hexValStr[1])
         }
-
-
-
-        const hex_1 = color_hex[0]
-        console.log(color_hex[1])
-        console.log(color_hex[2])
-        console.log(color_hex[3])
-
 
 
     return(
@@ -27,19 +24,19 @@ const ColorCubes = (props) => {
 
             <div className="container">
                     <div>
-                        <h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex[0]}
+                        <h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex_text[0]}
                         <HexCube hexColors = {color_hex[0]}/>
                     </div>
                     <div>
-                        <h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex[1]}
+                        <h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex_text[1]}
                         <HexCube hexColors = {color_hex[1]}/>
                     </div>
                     <div><h2>GHOSTS AND THEIR SHELLS </h2>
-                        <h3>DREAMING OF IMAGES</h3>{color_hex[2]}
+                        <h3>DREAMING OF IMAGES</h3>{color_hex_text[2]}
                         <HexCube hexColors = {color_hex[2]}/>
                     </div>
                     <div>
-                        <h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex[3]}
+                        <h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex_text[3]}
                         <HexCube hexColors = {color_hex[3]}/>
                     </div>
 
