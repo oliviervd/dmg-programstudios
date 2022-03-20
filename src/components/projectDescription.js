ab45292160789f1b5662c0b65c332fe8ef47d5e3
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 21:25:43 +0100

    update description.

--- src/components/projectDescription.js
+++ src/components/projectDescription.js
@@ -4,7 +4,7 @@ const ProjectDescription = () => {
             <h2>*** project description</h2>
             <p style={{fontSize: "15px"}}> MODELS FROM THE PAST FOR THE FUTURE takes the form of a speculative research into
                 alternative approaches and takes on the collection of Design Museum Gent. As an evolving platform and living archive it will
-                grow and serve as a testing ground for past, current and future research. If anything, it moves and shies
+                grow and serve as a sensing layer for past, current and future research. If anything, it moves and shies
                 away from classical approaches of indexing and in opening up museum collections online. It takes the collection database
                 as a starting point but remedies the wretch and clutch of its methodologies.
                 <br/>
