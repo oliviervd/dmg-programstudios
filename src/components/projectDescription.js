b09bcb3f156bb12678eabb1c63e98f09b91f59d6
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 20:02:24 +0100

    add init project description.

---
+++ src/components/projectDescription.js
@@ -0,0 +1,15 @@
+const ProjectDescription = () => {
+    return(
+        <div className="infoBoxLeft">
+            <h2>project description</h2>
+            <p> MODELS FROM THE PAST FOR THE FUTURE takes the form of a speculative research,
+                approach and take on the collection of Design Museum Gent. As an evolving platform it will grow in features in time.
+                and moves away from classic approaches when indexing and opening up museum collections. </p>
+            <h3>TRANSHISTORICAL</h3>
+
+            </div>
+
+    )
+}
+
+export default ProjectDescription;
