7d048548c9d24a24845232dd19b8d7823871c1c1
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 16:55:57 +0100

    add colorCubes.js

---
+++ src/components/colorCubes.js
@@ -0,0 +1,31 @@
+import React from "react";
+import object_colors from "../data/objectsColor.json";
+
+
+const ColorCubes = (props) => {
+
+        const selection = props.curation
+        const color_hex = [];
+        for (var hexNum=0; hexNum<5; ++hexNum) {
+            const x = selection[hexNum];
+            var _hexVal = object_colors[x]["HEX_values"].replace("['","").replace("']","").replace("'","").split(",")
+            color_hex.push(_hexVal)
+        }
+
+        console.log(color_hex)
+
+        return(
+            //<p>{props._hVal}</p> //todo: fetch hex values from masonry.js
+
+            <div className="container">
+                    <div><h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex[0]}</div>
+                    <div><h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex[1]}</div>
+                    <div><h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex[2]}</div>
+                    <div><h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex[3]}</div>
+                    <div><h2>GHOSTS AND THEIR SHELLS </h2><h3>DREAMING OF IMAGES</h3>{color_hex[4]}</div>
+            </div>
+        )
+
+}
+
+export default ColorCubes;
