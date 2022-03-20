50a1919c34c505daf3a6bd9d821269df9e859e6e
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 20:01:53 +0100

    reduce images to 4 to match new layout.

--- src/components/imageGenerator.js
+++ src/components/imageGenerator.js
@@ -5,7 +5,7 @@ const ImageGenerator = (props) => {
     const curatedSet = props.curatedSet;
     const _images = []
 
-    for (var imCount=0; imCount<5; ++imCount) {
+    for (var imCount=0; imCount<4; ++imCount) {
         const x = curatedSet[imCount]
         var _im = object_colors[x]["IIIF_image"].replace("['","").replace("']","").replace("'","").split(",")
         _images.push(_im);
@@ -18,7 +18,6 @@ const ImageGenerator = (props) => {
             <img alt="DREAMING OF IMAGES" src={_images[1][0].replace("/full/0/default.jpg","/500,/0/default.jpg")}></img>
             <img alt="DREAMING OF IMAGES" src={_images[2][0].replace("/full/0/default.jpg","/500,/0/default.jpg")}></img>
             <img alt="DREAMING OF IMAGES" src={_images[3][0].replace("/full/0/default.jpg","/500,/0/default.jpg")}></img>
-            <img alt="DREAMING OF IMAGES" src={_images[4][0].replace("/full/0/default.jpg","/500,/0/default.jpg")}></img>
         </div>
     )
 }
