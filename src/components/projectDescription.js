a2d3f726d8297458d7011877ef49c5f48bf89831
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 21:00:33 +0100

    update description.

--- src/components/projectDescription.js
+++ src/components/projectDescription.js
@@ -1,14 +1,19 @@
 const ProjectDescription = () => {
     return(
         <div className="infoBoxLeft">
-            <h2>project description</h2>
-            <p> MODELS FROM THE PAST FOR THE FUTURE takes the form of a speculative research,
-                approach and take on the collection of Design Museum Gent. As an evolving platform it will grow in features in time.
-                and moves away from classic approaches when indexing and opening up museum collections. </p>
-            <h3>TRANSHISTORICAL</h3>
+            <h2>*** project description</h2>
+            <p style={{fontSize: "15px"}}> MODELS FROM THE PAST FOR THE FUTURE takes the form of a speculative research into
+                alternative approaches and takes on the collection of Design Museum Gent. As an evolving platform it will
+                grow and serve as a testing ground for past, current and future research. If anything, it moves and shies
+                away from classical approaches of indexing and in opening up museum collections online. It takes the collection database
+                as a starting point but remedies the wretch and clutch of its methodologies.
+                <br/> <br/> The XENO-MORPHIC, the SENSORIAL, the TRANSHISTORICAL, the CURRENT and PAST-FUTURE are all ideologies
+                and perspectives that push us in devicing new kinds of interfaces that are perhaps more sensible to its users,
+                more aware of its surroundings and above all - kindle inspiration.
 
-            </div>
 
+            </p>
+        </div>
     )
 }
 
