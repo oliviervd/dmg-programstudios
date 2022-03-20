fe0b0327af4a31b22440c98df26b5ef2bd1d723b
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 21:28:06 +0100

    add footer

---
+++ src/components/xenoFooter.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const XenoFooter = () => {
+    return(
+        <div className="scroll-container">
+            <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
+            <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
+            <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
+            <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
+            <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
+        </div>
+    )
+};
+
+export default XenoFooter;
