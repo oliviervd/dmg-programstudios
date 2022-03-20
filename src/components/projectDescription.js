84c4f7a1b32cf9f4b4e6e3b134ed8417049a5770
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 21:08:16 +0100

    update description.

--- src/components/projectDescription.js
+++ src/components/projectDescription.js
@@ -3,13 +3,22 @@ const ProjectDescription = () => {
         <div className="infoBoxLeft">
             <h2>*** project description</h2>
             <p style={{fontSize: "15px"}}> MODELS FROM THE PAST FOR THE FUTURE takes the form of a speculative research into
-                alternative approaches and takes on the collection of Design Museum Gent. As an evolving platform it will
+                alternative approaches and takes on the collection of Design Museum Gent. As an evolving platform and living archive it will
                 grow and serve as a testing ground for past, current and future research. If anything, it moves and shies
                 away from classical approaches of indexing and in opening up museum collections online. It takes the collection database
                 as a starting point but remedies the wretch and clutch of its methodologies.
-                <br/> <br/> The XENO-MORPHIC, the SENSORIAL, the TRANSHISTORICAL, the CURRENT and PAST-FUTURE are all ideologies
-                and perspectives that push us in devicing new kinds of interfaces that are perhaps more sensible to its users,
+                <br/>
+
+                <h1><italic>_pull. transmit. fall in love, and break up again.</italic></h1>
+
+                <br/> The XENO-MORPHIC, the SENSORIAL, the TRANSHISTORICAL, the CURRENT and PAST-FUTURE are all ideologies
+                and strategies that push us in devicing new kinds of interfaces that are perhaps more sensible to its users,
                 more aware of its surroundings and above all - kindle inspiration.
+
+                <br/>
+
+                <h1>“break, mutate and build upon what is real and what isn't real.”
+                </h1>
 
 
             </p>
