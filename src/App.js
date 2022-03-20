dde55f18db54b1e2a91050a3e0fdd96efbeb5c70
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-20 12:34:05 +0100

    add context API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import XenoHeader from "./components/xeno"
 import MetaSelection from "./components/metaSelection"
 import MasonryGrid from "./components/masonry";
+import {spectrumProvider} from "./spectrumContext";
 
 /*
 STRUCTURE:
@@ -21,6 +22,9 @@ function App() {
         color: ""
     })
 
+    const [objectSelect, setObjectSelect] = useState(""); //todo: fetch and pass prop upstream from masonry.
+
+
     const handleSubmit = (c) => {
         c.preventDefault();
         console.log(colors)
@@ -28,22 +32,19 @@ function App() {
 
     return (
         <div>
-            <XenoHeader/>
-            <div className="container" id="imageRandom"></div>
-            <form onSubmit={handleSubmit}>
-                a spectrum of <br/>
-                <input onChange={(c) => setColors({...colors, color: c.target.value})}
-                       type="text"
-                       value={colors.color}/> <br/>
-                traversing an ocean of desire
-            </form>
-            <button onClick={MasonryGrid}>CLICK</button>
-            <MetaSelection col={colors.color}/>
-
-            {/*<h4>pull. transmit. fall in love, and break up again.</h4>*/}
-            {/*<button onClick = {MasonryGrid}>GENERATE</button>*/}
-            {/*<div className={"container"} id={"imageRandom"}>*/}
-
+            <spectrumProvider>
+                <XenoHeader/>
+                <div className="container" id="imageRandom"></div>
+                <form onSubmit={handleSubmit}>
+                    a spectrum of <br/>
+                    <input onChange={(c) => setColors({...colors, color: c.target.value})}
+                           type="text"
+                           value={colors.color}/> <br/>
+                    traversing an ocean of desire
+                </form>
+                <button onClick={MasonryGrid}>CLICK</button>
+                <MetaSelection col={colors.color}/>
+            </spectrumProvider>
         </div>
     );
 }
