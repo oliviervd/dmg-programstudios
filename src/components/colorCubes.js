3e588c5e74e26a0f7e1bf3aca2842e2831db7e7f
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-21 22:33:44 +0100

    clean up code

--- src/components/colorCubes.js
+++ src/components/colorCubes.js
@@ -1,11 +1,7 @@
 import React, {useState} from "react";
-import axios from 'axios';
 
 import object_colors from "../data/objectsColor.json";
 import HexCube from "../components/cube"
-
-const {translate} = require('deepl-translator');
-
 
 const ColorCubes = (props) => {
 
@@ -15,20 +11,15 @@ const ColorCubes = (props) => {
         const color_hex = [];
         const color_hex_text = [];
         const obj_titles_NL = [];
-        //const obj_titles_EN = [];
         for (var hexNum=0; hexNum<4; ++hexNum) {
             const x = selection[hexNum];
             var _hexVal = object_colors[x]["HEX_values"].replace("['","").replace("']","").replace("'","").split(",")
             const _hexValStr = object_colors[x]["HEX_values"].replace("['","").replace("']","").replace("'","").split("[")
             const _objTitle = object_colors[x]["title"]
-            //console.log(_objTitle)
-            //const _objTitleEn = translate("Vaas 83 met vogel", 'EN', 'NL')
-            //    .then(res => console.log(`Translation: ${res.translation}`))
-            //    .catch(console.error);
+
             color_hex.push(_hexVal);
             color_hex_text.push(_hexValStr[1]);
             obj_titles_NL.push(_objTitle);
-            //obj_titles_EN.push(_objTitleEn);
         }
 
     return(
