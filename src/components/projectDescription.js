4348b3386fdce23c1e46bf512e08e00373c37f47
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-21 18:10:01 +0100

    update description with modal

--- src/components/projectDescription.js
+++ src/components/projectDescription.js
@@ -1,3 +1,5 @@
+import Modal from "./modal/modal"
+
 const ProjectDescription = () => {
 
     function refreshPage() {
@@ -15,9 +17,12 @@ const ProjectDescription = () => {
                 as a starting point, it aims at remedying the wretch and clutch of its methodologies.
                 <br/>
 
-                <a><h1 onClick={refreshPage} className="presser hoverLinkEffect" ><italic>_pull. transmit. fall in love, and break up again.</italic></h1></a>
+                <h1><italic>_pull. transmit. fall in love, and break up again.</italic></h1>
 
-                <br/> The XENO-MORPHIC, the SENSORIAL & SENSIBLE, the TRANSHISTORICAL, the CURRENT and PAST-FUTURE are all ideologies
+                <br/> The <Modal text="XENO-MORPHIC" description="Meillisoux arrgues that one can only approach the contingent
+                nature of the world, which he calls 'hyperchaos', by creating a toolbox in which constant change could manifest itself.
+                Through this platform, Design Museum Gent makes visible, and encourages change to manifest and in doing so resonates
+                the layered, and altering nature of its collection, whether trough different readings in exhibitions or the lack thereof."></Modal>, the SENSORIAL & SENSIBLE, the TRANSHISTORICAL, the CURRENT and PAST-FUTURE are all ideologies
                 and strategies that push us in devicing new kinds of interfaces that are perhaps more sensible to its users,
                 more aware of its surroundings and above all - kindle inspiration.
 
