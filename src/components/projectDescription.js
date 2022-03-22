f843b1acdbfef4ade0b85457dd0112c3f4420286
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-22 23:42:48 +0100

    update description

--- src/components/projectDescription.js
+++ src/components/projectDescription.js
@@ -1,5 +1,8 @@
 import Modal from "./modal/modal"
 
+const chapters = "throughout different chapters this platform will coin topics that are related to the programming of the museum. " +
+    "chapter 1: good design is often related with the aesthetically pleasing. A first chapter approaches the collection from a sensorial perspective."
+
 const ProjectDescription = () => {
 
     function refreshPage() {
@@ -12,7 +15,7 @@ const ProjectDescription = () => {
             <p style={{fontSize: "15px"}}>MODELS FROM THE PAST FOR THE FUTURE is a site for speculative knowledge production
                 that is both reflective and performative in investigating and embracing the (as yet) unknown. It situates itself
                 in between where we are now and where we are going next. <ul> As an
-                    <bold className="underlined"> evolving platform</bold>, it seeks out alternative approaches and readings of the collection of Design museum Gent.</ul>
+                    <Modal modalID="modal2" modalLineID="modal2Line" text=" evolving platform" description={chapters}></Modal>, it seeks out alternative approaches and readings of the collection of Design museum Gent.</ul>
                 <ul>As a <bold className="underlined">living archive</bold>, it will grow and serve as a sensing layer for past, current and future research practices.</ul>
                 If anything, it moves and shies away from classical approaches of indexing and traditional ways of opening up museum collections online.
                 Although it takes the collection database as a starting point, it aims at remedying the wretch and clutch of its methodologies.
@@ -20,7 +23,7 @@ const ProjectDescription = () => {
 
                 <h1><italic>__pull. transmit. fall in love, and break up again.</italic></h1>
 
-                <br/> The <Modal text="XENO-MORPHIC" description="Meillisoux argues that one can only approach the contingent
+                <br/> The <Modal modalID="modal1" modalLineID="modal1Line" text="XENO-MORPHIC" description="Meillisoux argues that one can only approach the contingent
                 nature of the world, which he calls 'hyperchaos', by creating a toolbox in which constant change could manifest itself.
                 As a research platform, we aim to demonstrate the changing nature of museum collections by evoking the fictions
                 in which these changes occur. Whether by being part of the exhibition or the lack thereof..."></Modal>, the SENSORIAL & SENSIBLE, the TRANSHISTORICAL, the CURRENT and PAST-FUTURE are all ideologies
