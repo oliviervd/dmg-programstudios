90ee509c941311df43a00e9dc7ccad857b7f9884
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-23 10:57:56 +0100

    add titles and descriptons (if available) in English

--- src/components/colorCubes.js
+++ src/components/colorCubes.js
@@ -1,21 +1,44 @@
 import React from "react";
 
 import object_colors from "../data/objectsColor_10.json";
+import ldes_translations from "../data/ldes_dmg_translations.json"
+
 import HexCube from "../components/cube"
+
+function titleViaURI(uri) {
+    return ldes_translations.filter(
+        function(ldes_translations) {
+            return ldes_translations.URI === uri
+        }
+    )
+
+}
 
 const ColorCubes = (props) => {
 
         const selection = props.curation
         const color_hex = [];
-        const obj_titles_NL = [];
+        const obj_titles_EN = [];
+        const obj_desc_EN = [];
+
         for (var hexNum=0; hexNum<4; ++hexNum) {
             const x = selection[hexNum];
             var _hexVal = object_colors[x]["HEX_values"].replace("['","").replace("']","").replace("'","").split(",")
             const _hexValStr = object_colors[x]["HEX_values"].replace("['","").replace("']","").replace("'","").split("[")
             const _objTitle = object_colors[x]["title"]
 
+            const uri = object_colors[x]["URI"];
+            console.log(uri)
+
+            const title = titleViaURI(uri)
+            console.log(title)
+
+            const title_en = title[0]["title_en"];
+            const desc_en = title[0]["description_adlib_en"]
+
             color_hex.push(_hexVal);
-            obj_titles_NL.push(_objTitle);
+            obj_titles_EN.push(title_en);
+            obj_desc_EN.push(desc_en);
         }
 
     return(
@@ -27,18 +50,18 @@ const ColorCubes = (props) => {
                         <HexCube hexColors = {color_hex[0]}/>
                         <div className="dotLine"></div>
                         <div className="titleBox">
-                            <h2 className="titleBoxTitle">**{obj_titles_NL[0]}</h2>
+                            <h2 className="titleBoxTitle">**{obj_titles_EN[0]}</h2>
                         </div>
-                        <h3 className="rowScroll"></h3>
+                        <h3 className="rowScroll">{obj_desc_EN[0]}</h3>
                     </div>
                     <div>
 
                         <HexCube hexColors = {color_hex[1]}/>
                         <div className="dotLine"></div>
                         <div className="titleBox">
-                            <h2 className="titleBoxTitle underlined">**{obj_titles_NL[1]}</h2>
+                            <h2 className="titleBoxTitle underlined">**{obj_titles_EN[1]}</h2>
                         </div>
-                        <h3 className="rowScroll"></h3>
+                        <h3 className="rowScroll">{obj_desc_EN[1]}</h3>
 
 
                     </div>
@@ -47,9 +70,9 @@ const ColorCubes = (props) => {
                         <HexCube hexColors = {color_hex[2]}/>
                         <div className="dotLine"></div>
                         <div className="titleBox">
-                            <h2 className="titleBoxTitle">**{obj_titles_NL[2]}</h2>
+                            <h2 className="titleBoxTitle">**{obj_titles_EN[2]}</h2>
                         </div>
-                        <h3 className="rowScroll"></h3>
+                        <h3 className="rowScroll">{obj_desc_EN[2]}</h3>
 
 
                     </div>
@@ -58,9 +81,12 @@ const ColorCubes = (props) => {
                         <HexCube hexColors = {color_hex[3]}/>
                         <div className="dotLine"></div>
                         <div className="titleBox">
-                            <h2 className="titleBoxTitle underlined">**{obj_titles_NL[3]}</h2>
+                            <h2 className="titleBoxTitle underlined">**{obj_titles_EN[3]}</h2>
                         </div>
-                        <h3 className="rowScroll"></h3>
+                        <div className='rowScroll'>
+                            <h3>{obj_desc_EN[3]}</h3>
+                        </div>
+
 
                     </div>
 
