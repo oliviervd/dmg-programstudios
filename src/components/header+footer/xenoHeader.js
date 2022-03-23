9192119398e4952bb31f132e2ca8fc383cf848bf
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-23 01:03:47 +0100

    update header.

--- src/components/header+footer/xenoHeader.js
+++ src/components/header+footer/xenoHeader.js
@@ -10,10 +10,10 @@ const XenoHeader = () => {
                 <h1 className="scroll-text"> models from the past for the future | </h1>
             </div>
             <div className="scroll-container">
-                <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the anti-museum | SPECTRUM | the sensible | </h2>
-                <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the anti-museum | SPECTRUM | the sensible |</h2>
-                <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the anti-museum | SPECTRUM | the sensible |</h2>
-                <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the anti-museum | SPECTRUM | the sensible |</h2>
+                <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the sensible | </h2>
+                <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the sensible |</h2>
+                <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the sensible |</h2>
+                <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the sensible |</h2>
 
             </div>
             <div className="scroll-container">
