9071d810c6c6ecbd63802bf1c5750f1cdf9afac7
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-25 17:52:43 +0100

    update modal layout + text

--- src/components/projectDescription.js
+++ src/components/projectDescription.js
@@ -2,9 +2,10 @@ import Modal from "./modal/modal"
 import React from "react"
 
 const chapters = "MODELS FROM THE PAST FOR THE FUTURE serves as a counter-proposal for the accelerated cultural production" +
-    "that is noticeable in  museums around the world. Taking time to find different means of approaching a museum collection " +
-    "this project can be read as a critical inquiry into the way collections are usualy displayed and how this manifested itself" +
-    "throughout time."
+    "that is noticeable in museums around the world and questions the role of the museum as a device that has to continuously " +
+    "reinvent itself in offering new experiences. Taking time to find different means of approaching a museum collection " +
+    "this project can be read as a critical inquiry into the way collections are usually displayed, looking for alternative ways of production " +
+    "that are perhaps more sensible on a planetary scale in terms of our surroundings."
 
 const xenomorphic = "Meillisoux argues that one can only approach the contingent " +
     "nature of the world, which he calls 'hyperchaos', by creating a toolbox in which constant change could manifest itself." +
@@ -25,9 +26,9 @@ const ProjectDescription = () => {
         <div className="infoBoxLeft">
             <h1 className="underlined" style={{fontSize: "20px"}}>___PROJECT DESCRIPTION</h1>
             <p style={{fontSize: "15px"}}>MODELS FROM THE PAST FOR THE FUTURE is a site for speculative knowledge production
-                that is both <Modal  modalID="modal2" modalLineID="modal2Line" text="reflective" description={reflective}/> and performative in investigating and embracing the (as yet) unknown. It situates itself
-                in between where we are now and where we are going next. <ul> As an
-                    <Modal modalID="modal3" modalLineID="modal3Line"  text=" evolving platform" description={chapters}></Modal>, it seeks out alternative approaches and readings of the collection of Design museum Gent.</ul>
+                that is both <Modal  modalID="modal2 conicBackdrop" modalLineID="modal2Line" text="reflective" description={reflective}/> and performative in investigating and embracing the (as yet) unknown. It situates itself
+                in between where we are now and where we are going next and interweaves history with visions of the future. <ul> As an
+                    <Modal modalID="modal3 conicBackdrop" modalLineID="modal3Line"  text=" evolving platform" description={chapters}></Modal>, it seeks out alternative approaches and readings of the collection of Design museum Gent.</ul>
                 <ul>As a <bold className="underlined">living archive</bold>, it will grow and serve as a sensing layer for past, current and future research practices.</ul>
                 If anything, it moves and shies away from classical approaches of indexing and traditional ways of opening up museum collections online.
                 Although it takes the collection database as a starting point, it aims at remedying the wretch and clutch of its <Modal text="methodologies">methodologies.</Modal>
@@ -35,7 +36,7 @@ const ProjectDescription = () => {
 
                 <h1><italic>__pull. transmit. fall in love, and break up again.</italic></h1>
 
-                <br/> The <Modal modalID="modal1" modalLineID="modal1Line" text="XENO-MORPHIC" description={xenomorphic}></Modal>, the <Modal modalID="modal4" text="the SENSORIAL"></Modal> & SENSIBLE, the TRANSHISTORICAL, the CURRENT and PAST-FUTURE are all ideologies
+                <br/> The <Modal modalID="modal1 conicBackdrop" modalLineID="modal1Line" text="XENO-MORPHIC" description={xenomorphic}></Modal>, the <Modal modalID="modal4" text="the SENSORIAL"></Modal> & SENSIBLE, the TRANSHISTORICAL, the CURRENT and PAST-FUTURE are all ideologies
                 and strategies that propel us in devising new kinds of interfaces that are perhaps more sensible to its users,
                 more conscious of its surroundings and above all - foster inspiration.
 
