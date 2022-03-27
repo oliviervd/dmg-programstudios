dbedba0b4f21a0090cac551b6bfd088bef1ddee9
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-03-27 14:27:31 +0200

    from chapter to model.

--- src/components/header+footer/xenoHeader.js
+++ src/components/header+footer/xenoHeader.js
@@ -17,11 +17,11 @@ const XenoHeader = () => {
 
             </div>
             <div className="scroll-container">
-                <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
-                <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
-                <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
-                <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
-                <h3 className="scroll-text"> chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial || chapter 1: the sensorial ||</h3>
+                <h3 className="scroll-text"> model 1: the sensorial || model 1: the sensorial || model 1: the sensorial || model 1: the sensorial ||</h3>
+                <h3 className="scroll-text"> model 1: the sensorial || model 1: the sensorial || model 1: the sensorial || model 1: the sensorial ||</h3>
+                <h3 className="scroll-text"> model 1: the sensorial || model 1: the sensorial || model 1: the sensorial || model 1: the sensorial ||</h3>
+                <h3 className="scroll-text"> model 1: the sensorial || model 1: the sensorial || model 1: the sensorial || model 1: the sensorial ||</h3>
+                <h3 className="scroll-text"> model 1: the sensorial || model 1: the sensorial || model 1: the sensorial || model 1: the sensorial ||</h3>
             </div>
         </div>
     )
