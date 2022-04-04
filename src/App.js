a4236193398f3e553a336277084b24964d2a6f36
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-04-04 18:01:08 +0200

    close #7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import XenoHeader from "./components/header+footer/xenoHeader"
 import SpectreMain from "./components/spectre"
 import ProjectDescription from "./components/projectDescription";
@@ -7,17 +7,34 @@ function App() {
 
     const _objectNum = 4;
 
+    const [sideActive, setSideActive] = useState(true);
+
+    function openSide() {
+        console.log("MTF STOP HIDING ME")
+        setSideActive(!sideActive);
+    }
+
 
     return (
         <div>
-                <XenoHeader/>
-
+            <XenoHeader/>
+            <div className="superContainer">
+                {sideActive &&
+                    <ProjectDescription/>
+                }
+                <div className="mainContainerAlt" onClick={openSide}>
+                    <div className="dotLine"/>
+                    <SpectreMain num={_objectNum}/>
+                    <div className="dotLine"/>
+                </div>
+            </div>
+                {/*<XenoHeader/>
                 <div className="mainContainer">
                     <ProjectDescription/>
                     <div className="dotLine"/>
                     <SpectreMain num={_objectNum}/>
                     <div className="dotLine"/>
-                </div>
+                </div>*/}
         </div>
     );
 }
