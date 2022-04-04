6ec3de5dadbe65ece3610f2aed84405dbeb93584
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-04-04 17:58:52 +0200

    adapt text

--- src/components/models/model1.js
+++ src/components/models/model1.js
@@ -3,22 +3,23 @@ import React from "react";
 const model1_intro = "Designers today are transforming our relationship with the natural world. In rethinking the consequences " +
     "of our actions on the environment they are challenging basic assumptions that relate to modes of production, " +
     "authorship as well as redefining our relationship to nature through speculative forms of design, hinting at new possible outcomes. " +
-    "Yet, we remain confronted with resource hungry museums that seem to contradict the very models we elevate in our exhibitions on sustainable and sensible design. " +
-    "This raises questions on expanding the role museum as a model, a care-taking institution, " +
-    "not only of its collection but also for its community and environment."
+    "Yet, we remain confronted with resource hungry museums that seem to contradict the very message we send out to our public in our exhibitions;** " +
+    "This raises questions on how we can transform the museum model towards a care-taking institution. Not only of its " +
+    "collections but also for its community and environment. In order to do so, we need to critically address all dimensions " +
+    "of the museum, its full metabolism, starting at its core, in its collecting and archival practices."
 
-const model1_0 = "Collections can be indexed in many ways based on the information that can be extracted from their " +
-    "collection databases. One form of indexation that is common and often referred to as the go-to entry point when " +
-    "navigating through large collections is the object and its author. Although this seems a logical way to approach " +
-    "design, we have to be wary of how this mode of looking also imposes a hierarchical structure of power that " +
-    "transcends the qualities of the object itself. And how this reign of authorship is problematic when taking into " +
-    "consideration the ongoing debate on sustainable forms of production as it implies a certain constant rethinking and production of the new."
+const model1_0 = "Collections  can be indexed in many ways based on the information that can be  extracted from the collection database. " +
+    "One form of indexation that is  common and often referred to as the go-to entry point when navigating " +
+    "through large collections is the object and its author.  Although this  seems a logical way to approach design, " +
+    "we have to be wary of how this  model evokes power structures that might not be sensible in terms of the debates that are topical today, " +
+    "in which the notion of authorship is questioned in terms of its ecological impact as it implies a certain constant rethinking and production of the new. " +
+    "The urge is now to reposition and seek alternative ways of knowledge productions that are concerned with the debates that are topical today, the challenges of the 21st century. "
 
-const model1_1 =
-    "As a counter-proposal and alternative curatorial model, SENSE AND SENSIBILITY, (re-)negotiates form as a base principle" +
-    " for design and looks into for example the role of colour as a sensorial mode of learning and relates this to current " +
-    "practices in the museum. In doing so, I don't wish to negate the role of authorship but rather establish a mutual " +
-    "relationship based on scales and values that are more appropriate for our current times."
+const model1_1 = "As a counter-proposal and exploratory curatorial model, SENSE AND  SENSIBILITY, (re-)negotiates form " +
+    "as a base principle for design, looking beyond the status of recognized authorship and undocumented artifacts. " +
+    "This does not mean to embrace anonymity, nor the  wish to negate the role and importance of the designer but rather " +
+    "to establish a mutual relationship and recognition of the other based on scales and values that are more appropriate for our current times."
+
 
 const Model1 = () => {
     return(
