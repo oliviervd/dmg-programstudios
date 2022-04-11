d65146aa663786337108e11b0bdc4c75e2cc21a3
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-04-11 13:03:33 +0200

    update text

--- src/components/models/model1.js
+++ src/components/models/model1.js
@@ -9,17 +9,16 @@ const Model1 = () => {
                 <div>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_italic_master_web"}}>
                         What does it mean today to act with care? How do we take care of each other and what can the role of the sustainable museum be in all this?
-                        How can we, as a care-taking institution, not only agitate about society but also agitate against society.
-                        How can we effectively live together, not only on a human scale, but also on a larger, more sustainable scale?
-                        For defining Notions of production, authorship and symbiosis are critically approached in forms of speculative and research design.
-                        All contribute to defining possible future perspectives that transcend our traditional models and world views.
+                        How can we, as a care-taking institution, not only agitate about society but also agitate for society. How can we think of new forms to effectively live together,
+                        not only on a human scale, but also on a larger, more sustainable scale? Defining possible alternative forms of coexistence
+                        and the effects of human actions on the climate have become an important starting point for many designers today in their practice.
+                        Notions of production, authorship and symbiosis are critically approached in forms of speculative and research design.
                     </div>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_italic_master_web"}}>
-                        The social role of the museum consists not only in opening up these new models and design principles but also in acting
-                        upon them as a heritage institution in society. If not, we as a museum are in danger of lapsing into a form of facadism
-                        in which programming about sustainability is not also sustainable. The latter is a major challenge that prevails internationally.
-                        In this sense, making the institution sustainable implies a critical reprogramming of the museum and metabolism as a whole.
+                        The social role of the museum consists not only in opening up these new models and design principles but also in acting upon them as a heritage institution that is part of society.
+                        If not, we as a museum are in danger of lapsing into a form of facadism in which programming about sustainability is not also sustainable.
+                        The latter is a major challenge that prevails internationally. In this sense, making the institution sustainable implies a critical reprogramming of the museum and its metabolism as a whole.
                     </div>
                     <br/>
                     <h2 className="left box" style={{borderLeft: "3px solid black", paddingLeft: "5px"}}>__Who are we taking care for? <br/> On the Logic of Collecting and Sovereignty towards a Collection of Care.</h2>
@@ -31,25 +30,23 @@ const Model1 = () => {
                     </div>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        Design museum Gent owns a 19th century catalogue  consisting of studies made by a skilled colorist. Although only displayed once, this book used to belong to library of Henry van de Velde,
-                        who might have been inspired by this book for one or more of his textile designs. This first model takes this idea of the colorbook, as a source for insipiration,
-                        and makes use of technology to make speculative between works from the collection in relation to the book.
-                        By extracting the colors in the photographs, we can create a network in which objects are close to one another based on color.
-                        As an act of speculation that share this notion of inspiration.
+                        Design museum Gent owns a 19th century catalogue consisting of studies made by a skilled colorist. Although only displayed once, this book used to be part of the library of Henry van de Velde,
+                        who might have been inspired by the samples in this book for one or more of his textile designs. This first model takes this idea of the color book, as a source for inspiration,
+                        and makes use of technology to make speculative between works from the collection in relation to the book. By extracting the colors in the photographs,
+                        we can create a network in which objects are close to one another based on color. Within this speculative realm,
+                        all objects mutually coexist within a fictitious framework that is both transhistorical and transdisciplinary. In which sovereignty is replaced with notions care.
                     </div>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        This re-organizing of the collection based on samples made by unknown hand, evokes the culture of the anonymous and at the same time questions
-                        the importance of authorship as an inherent quality that defines visibility or the lack thereof. Approaching the collection from this notion
-                        of connectivity and relationships rather than a source of information shifts the emphasis from the institutional to the rather human and is
-                        reminiscent to the early history of the museum.  Can such an approach help us in finding ways that aid in the desacralization of a museum collection?
+                        This re-organizing of the collection based on samples made by unknown hand, evokes the culture of the anonymous and at the same time questions the importance of authorship as an inherent quality that defines visibility or the lack thereof.
+                        Approaching the collection from this notion of connectivity and relationships rather than a source of information shifts the emphasis from the institutional to the rather human and is reminiscent to the early history of the museum.
+                        Can such an approach help us in finding ways that aid in the desacralization of a museum collection?
                     </div>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        Authorship and visibility often go hand in hand. As a museum, we need to be aware of how we may or may not further perpetuate these dominant forms of authorship.
-                        As a counter-proposal and exploratory curatorial model, SENSE AND SENSIBILITY, (re-)negotiates form as a base principle for design, looking beyond the status of
-                        recognized authorship and undocumented artifacts. This does not mean to embrace anonymity, nor the wish to negate the role and importance of the designer but
-                        rather to establish a mutual relationship and recognition of the other based on scales and values that are more appropriate for our current times
+                        Authorship and visibility often go hand in hand. As a museum, we need to be aware of how we may or may not further perpetuate these dominant forms of authorship. As a counter-proposal and exploratory curatorial model,
+                        SENSE AND SENSIBILITY, (re-)negotiates form as a base principle for design, looking beyond the status of recognized authorship and undocumented artifacts. This model is not intended as an argument for or against authorship.
+                        I does however seek to establish a mutual relationship and recognition of the “other” based on scales and values that are more appropriate for the challenges that define our current times.
                     </div>
 
                 </div>
