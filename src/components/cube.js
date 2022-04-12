1f53276a87dcaa5bec06b12ca2c6fdac54ce0b01
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-04-12 10:49:47 +0200

    clean code

--- src/components/cube.js
+++ src/components/cube.js
@@ -18,19 +18,6 @@ const HexCube = (props) => {
                 <div className ="hexCube__single" style={{backgroundColor: hex_list[8].replace(" '","").replace("'","")}}>{hex_list[8].replace(" '","").replace("'","")}</div>
                 <div className ="hexCube__single" style={{backgroundColor: hex_list[9].replace(" '","").replace("'","")}}>{hex_list[9].replace(" '","").replace("'","")}</div>
             </div>
-            {/*<div className="hexCubeNames">
-                <p> {color_names[0].replace("[['","").replace("'","")}</p>
-                <p> {color_names[1].replace("'","").replace("'","")}</p>
-                <p> {color_names[2].replace("'","").replace("'","")}</p>
-                <p> {color_names[3].replace("'","").replace("'","")}</p>
-                <p> {color_names[4].replace("'","").replace("'","")}</p>
-                <p> {color_names[5].replace("'","").replace("'","")}</p>
-                <p> {color_names[6].replace("'","").replace("'","")}</p>
-                <p> {color_names[7].replace("'","").replace("'","")}</p>
-                <p> {color_names[8].replace("'","").replace("'","")}</p>
-                <p> {color_names[9].replace("]]","")}</p>
-
-            </div>*/}
         </div>
 
     )
