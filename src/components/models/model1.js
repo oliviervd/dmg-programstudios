5a34b8baa637241e7c846c9c7c6400b989362552
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-04-12 10:49:35 +0200

    close #21

--- src/components/models/model1.js
+++ src/components/models/model1.js
@@ -8,46 +8,46 @@ const Model1 = () => {
                 <div/>
                 <div>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_italic_master_web"}}>
-                        What does it mean today to act with care? How do we take care of each other and what can the role of the sustainable museum be in all this?
-                        How can we, as a care-taking institution, not only agitate about society but also agitate for society. How can we think of new forms to effectively live together,
-                        not only on a human scale, but also on a larger, more sustainable scale? Defining possible alternative forms of coexistence
-                        and the effects of human actions on the climate have become an important starting point for many designers today in their practice.
-                        Notions of production, authorship and symbiosis are critically approached in forms of speculative and research design.
+                        “When is an object socially or ecologically responsible?” , this question was central to one of the tracks within Object Stories,
+                        the permanent collection presentation of the Design Museum Ghent, for 4 years. Although there is an increased focus on sustainability and social concern today -
+                        this phenomenon is not new in the design world per se. Throughout history, various design strategies have emerged that critically address the impact of our actions on the climate and,
+                        in response, define new production methods and/or forms of society.
                     </div>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_italic_master_web"}}>
-                        The social role of the museum consists not only in opening up these new models and design principles but also in acting upon them as a heritage institution that is part of society.
-                        If not, we as a museum are in danger of lapsing into a form of facadism in which programming about sustainability is not also sustainable.
-                        The latter is a major challenge that prevails internationally. In this sense, making the institution sustainable implies a critical reprogramming of the museum and its metabolism as a whole.
+                        Marjan van Helvert defined “Responsible Objects” as objects that relate responsibly, consciously and with care to their environment.
+                        In line with this, the question arises today how we as an institution can also make our contribution within this shifting society.
+                        Not only by disseminating and opening up these good examples of design, but also by making the critical reflection ourselves of how we can act in a more caring manner.
+                        An institution that does not only act about society, but also for and in society. If not, we risk lapsing into a form of façadism in which programming about sustainability
+                        is not also sustainable. The latter is a major challenge that prevails and is being addressed internationally within the (touring) exhibition sector.
+                        In this sense, making the institution sustainable also implies a critical reprogramming of the museum and its metabolism as a whole.
                     </div>
                     <br/>
                     <h2 className="left box" style={{borderLeft: "3px solid black", paddingLeft: "5px"}}>__Who are we taking care for? <br/> On the Logic of Collecting and Sovereignty towards a Collection of Care.</h2>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        When we talk about the museum and its metabolism, one of the most important organs remains the collection and its management.
-                        However, these actions, often invisible to the public, and the choices made in them also carry consequences.
-                        How do we define a collection of care?
+                        As a museum, the collection obviously remains one of the most prominent and essential organs within the museum's metabolism.
+                        How can we move past taking care of our collection, towards a collection of care?
+                        The way the museum acquires collection objects is varying, some objects are acquired directly from the designer, others are acquired as gifts,
+                        thus already prior belonging to another collection. The latter transaction is characterized by the (intimate) transfer of responsibility and care for a certain object.
+                        The latter occured in the acquisition of the "sample book with paint recipes", a 19th-century catalog of samples that belonged to the personal library of Henry van de Velde.
+                        It is possible that this book was used as a source of inspiration when designing one or more of his textile designs.
+                        Taking this model, in which one object inspires the other, as our starting point,
+                        we are investigating how technology can be used to establish speculative relationships between
+                        the samples in this book on one hand and the digitized objects in our collection on the other.
+                        By means of color extraction, a speculative network is created in which objects relate to one another based on their color palette.
+                        This reorganization of the collection based on samples made and collected by an unknown colorist,
+                        evokes a "culture of anonymity" while also seeking other approaches to our collection that move beyond the dominant form of authorship.
+                        An inherent quality of the work that often determines the visibility and/or invisibility of an object.
+
                     </div>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        Design museum Gent owns a 19th century catalogue consisting of studies made by a skilled colorist. Although only displayed once, this book used to be part of the library of Henry van de Velde,
-                        who might have been inspired by the samples in this book for one or more of his textile designs. This first model takes this idea of the color book, as a source for inspiration,
-                        and makes use of technology to make speculative between works from the collection in relation to the book. By extracting the colors in the photographs,
-                        we can create a network in which objects are close to one another based on color. Within this speculative realm,
-                        all objects mutually coexist within a fictitious framework that is both transhistorical and transdisciplinary. In which sovereignty is replaced with notions care.
+                        This approach of the collection as a social network, - a speculative system that connects in a transhistorical and transdisciplinary way -
+                        transcends the art historical and institutionalized view - and also refers to the early history of the museum where it functioned as a model museum;
+                        a place with the social purpose of inspiring craftsmen by presenting good models or forms of design.
                     </div>
-                    <br/>
-                    <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        This re-organizing of the collection based on samples made by unknown hand, evokes the culture of the anonymous and at the same time questions the importance of authorship as an inherent quality that defines visibility or the lack thereof.
-                        Approaching the collection from this notion of connectivity and relationships rather than a source of information shifts the emphasis from the institutional to the rather human and is reminiscent to the early history of the museum.
-                        Can such an approach help us in finding ways that aid in the desacralization of a museum collection?
-                    </div>
-                    <br/>
-                    <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        Authorship and visibility often go hand in hand. As a museum, we need to be aware of how we may or may not further perpetuate these dominant forms of authorship. As a counter-proposal and exploratory curatorial model,
-                        SENSE AND SENSIBILITY, (re-)negotiates form as a base principle for design, looking beyond the status of recognized authorship and undocumented artifacts. This model is not intended as an argument for or against authorship.
-                        I does however seek to establish a mutual relationship and recognition of the “other” based on scales and values that are more appropriate for the challenges that define our current times.
-                    </div>
+                    <br/><br/>
 
                 </div>
 
