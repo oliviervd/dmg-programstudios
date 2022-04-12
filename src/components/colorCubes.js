4f85b968e15b4fa1e74ecd1a6549362943d3c774
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-04-12 14:28:37 +0200

    change init state.

--- src/components/colorCubes.js
+++ src/components/colorCubes.js
@@ -17,9 +17,9 @@ const ColorCubes = (props) => {
 
     // open close object description
 
-    const [objectDescription0, setObjectDescription0] = useState(true);
-    const [objectDescription1, setObjectDescription1] = useState(true);
-    const [objectDescription2, setObjectDescription2] = useState(true);
+    const [objectDescription0, setObjectDescription0] = useState(false);
+    const [objectDescription1, setObjectDescription1] = useState(false);
+    const [objectDescription2, setObjectDescription2] = useState(false);
 
 
     function openDescription0() {
