31eff40dd30d7491c2a4023c24eb75fc354ba30c
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-04-22 10:33:54 +0200

    ref #37 - add title to image modal.

--- src/components/imageGenerator.js
+++ src/components/imageGenerator.js
@@ -1,11 +1,25 @@
 import React, {useState} from "react";
 import ImageViewer from "./modal/imageModal";
 
+import ldes_translations from "../data/postgres_ldes_dmg_translations_ldes_dmg.json"
+
+function LDESViaURI(uri) {
+    return ldes_translations.filter(
+        function(ldes_translations) {
+            return ldes_translations.URI === uri
+        }
+    )
+
+}
+
+
 const ImageGenerator = (props) => {
     const curatedSet = props.curatedSet;
     const object_colors = props.data;
     const objCount = props.num;
-    const _images = []
+    const _images = [];
+    const _titles = [];
+
 
     const[imageViewer1, setImageViewer1] = useState(false);
     const[imageViewer2, setImageViewer2] = useState(false);
@@ -13,21 +27,25 @@ const ImageGenerator = (props) => {
 
     for (var imCount=0; imCount<objCount; ++imCount) {
         const x = curatedSet[imCount] // TODO: fix this goes above length of array (number of images)
+        const uri = object_colors[x]["URI"];
+        const _LDES = LDESViaURI(uri)
         let _im = object_colors[x]["IIIF_image"].replace("['","").replace("']","").replace("'","").split(",")
         _images.push(_im);
+        _titles.push(_LDES[0]["title_en"])
     }
 
     return(
         <div className="container" id="imageRandom">
 
             <img className="__img" alt="DREAMING OF IMAGES" onClick={()=>{setImageViewer1(true)}}  src={_images[0][0].replace("/full/0/default.jpg", "/1000,/0/default.jpg")}/>
-                <ImageViewer open={imageViewer1} onClose={() => setImageViewer1(false)} image={_images[0][0]}/>
+                <ImageViewer open={imageViewer1} onClose={() => setImageViewer1(false)} image={_images[0][0]} _title={_titles[0]}/>
 
             <img className="__img" alt="DREAMING OF IMAGES" onClick={()=>{setImageViewer2(true)}}  src={_images[1][0].replace("/full/0/default.jpg", "/1000,/0/default.jpg")}/>
-                <ImageViewer open={imageViewer2} onClose={() => setImageViewer2(false)} image={_images[1][0]}/>
+                <ImageViewer open={imageViewer2} onClose={() => setImageViewer2(false)} image={_images[1][0]} _title={_titles[1]}/>
 
             <img className="__img" alt="DREAMING OF IMAGES" onClick={()=>{setImageViewer3(true)}} src={_images[2][0].replace("/full/0/default.jpg", "/1000,/0/default.jpg")}/>
-                <ImageViewer open={imageViewer3} onClose={()=> setImageViewer3(false)} image={_images[2][0]}/>
+                <ImageViewer open={imageViewer3} onClose={()=> setImageViewer3(false)} image={_images[2][0]} _title={_titles[2]}/>
+
 
         </div>
     )
--- src/components/modal/imageModal.js
+++ src/components/modal/imageModal.js
@@ -1,13 +1,16 @@
 import React from "react"
 
-const ImageViewer = ({open, onClose, image}) => {
+const ImageViewer = ({open, onClose, image, _title}) => {
     if(!open) return null
     return(
         <div className="imageViewer__modal">
-            <div/>
+            <div className="imageViewer__modal__title">
+                <h2 className="imageViewer__modal__title-inner">{_title}</h2>
+            </div>
             <img className="img__large" src={image}></img>
             <div>
                 <div className="closeButton__modal" onClick={onClose}>▒▒✖▒▒</div>
+                <p></p>
             </div>
         </div>
     )
