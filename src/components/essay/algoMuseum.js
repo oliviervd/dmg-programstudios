2d8629c75c6cd31990e63eccec44678eb8d06741
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-04 21:06:03 +0200

    add essay page.

---
+++ src/components/essay/algoMuseum.js
@@ -0,0 +1,21 @@
+import React from "react"
+import XenoHeader from "../header+footer/xenoHeader";
+
+const EssayAlgoMuseum = () => {
+    return(
+        <div>
+            <div className="headerContainer">
+                <XenoHeader/>
+                <div className="languages_button_box">
+                    <div className="button-lang">NL</div>
+                    <div className="button-lang">EN</div>
+                </div>
+                <div>
+                    <crypto--h1 className="center underlined">THE MUSEUM AND THE COMPUTATIONAL</crypto--h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default EssayAlgoMuseum;
