9af236b15de26628ad625d5de9569c8b5a5a4d2e
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-07 09:25:00 +0200

    update essay MODEL 1: SENSE AND SENSIBILITY.

--- src/components/models/model1.js
+++ src/components/models/model1.js
@@ -10,10 +10,10 @@ const Model1 = () => {
                     <div></div>
                     <ol>
                         <li>
-                            <div id="object_stories"> <em>Object Stories </em> was part of the permanent collection presentation.
-                                Built out of 6 layers (<em>Commisions & Commisioners, Making Matters, Curiosity, Responsible Objects,
+                            <div id="object_stories"> <em>Object Stories </em> offered several looks on the collection of the museum.
+                                Consisting out of 6 (thematic) layers (<em>Commisions & Commisioners, Making Matters, Curiosity, Responsible Objects,
                                     Bauhaus 100!</em> and <em>Children's Choices</em>) it highlighted
-                                certain objects from the collection from a certain point of view.
+                                featured around 200 objects from the collection of which some were highlighted from certain point of view.
                             </div>
                         </li>
                         <li>
@@ -39,8 +39,11 @@ const Model1 = () => {
                             <div id="models_museum">
                                 The museum was established in 1903 as a "models museum" (<em>modellenmuseum</em> or <em>musee des modelles</em>).
                                 The collection comprised of good examples of various crafts and trades to inspire artists and craftsmen and
-                                provide support for their training.<img src="https://api.collectie.gent/iiif/image/iiif/2/b7a376735df545a775899c6efb23f808-MA_SCMS_FO_00683.tif/624,661,1992,1423/full/0/default.jpg"></img><a href="#models_museum_ref" aria-label="Back to content">↩</a>
+                                provide support for their training.<a href="#models_museum_ref" aria-label="Back to content">↩</a>
                             </div>
+                        </li>
+                        <li>
+                            <img src="https://api.collectie.gent/iiif/image/iiif/2/b7a376735df545a775899c6efb23f808-MA_SCMS_FO_00683.tif/624,661,1992,1423/full/0/default.jpg"></img>
                         </li>
 
                     </ol>
@@ -49,7 +52,7 @@ const Model1 = () => {
 
                 <article>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_italic_master_web"}}>
-                        <p style={{fontSize: "15px", fontFamily: "P-ACR-AGON-Regular"}}>RESPONSIBLE</p>. “When is an object socially or ecologically responsible?<a href="#object_stories" aria-describedby="footnote-label" id="object_stories_ref">"</a>
+                        <p style={{fontSize: "15px", fontFamily: "P-ACR-AGON-Regular"}}>RESPONSIBLE</p>“When is an object socially or ecologically responsible?<a href="#object_stories" aria-describedby="footnote-label" id="object_stories_ref">"</a>
                         For four years this question was central to one of the tracks in Object Stories, the permanent presentation
                         of the collection of Design Museum Gent. The objects on display (within this specific track) all in their
                         own way demonstrated that although there is an increased focus on sustainability and social concern today,
@@ -74,24 +77,35 @@ const Model1 = () => {
                     <h2 className="left box" style={{borderLeft: "3px solid black", paddingLeft: "5px"}}>__Who are we taking care for? <br/> On the Logic of Collecting and Sovereignty towards a Collection of Care.</h2>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        <p style={{fontSize: "15px", fontFamily: "P-ACR-AGON-Regular"}}>METABOLISM</p>. What if we tried to alter our metabolism in such a way that the core task of the museum of taking
-                        care of the collection (conservation/preservation) is extended to the sensible act of supporting a <u>collection of care</u>?
-                        For is not the process of expanding the collection through acquisition essentially a transfer of caring for an object from one person to the other?
-                        Let’s take for example the acquisition of the "sample book with paint recipes", a 19th-century catalog of samples that used to belong to the personal library of Henry van de Velde,
-                        before it was gifted to the museum. It is possible that this book was used as a source of inspiration
-                        when designing one or more of his textile designs. We are now taking care of this object by preserving it,
-                        yet it has almost never been on display in the museum, which begs the question, who are we taking care for?
+                        <p style={{fontSize: "15px", fontFamily: "P-ACR-AGON-Regular"}}>METABOLISM</p>Could we imagine
+                        ways of changing the metabolism of the museum in such a way that core tasks of the museum such as
+                        taking care of the collection (conservation/preservation) is reformulated into the sensible act of supporting a  <u>collection of care</u>?
+                        Could we not agree that the process of expanding the collection through acquisition is essentially
+                        a transfer of “caring for” an object from one person to the other? Defining what is meant by
+                        “to take care for”, as well as the transformation of caring for a collection into a collection of
+                        care may help us as a museum in our transformation into a sustainable and caring institution.
+                        Let’s take for example the acquisition of the "sample book with paint recipes",
+                        a 19th-century catalog of samples that used to belong to the personal library of Henry van de Velde,
+                        before it was gifted to the museum. It is plausible that this book was used as a source of inspiration
+                        by Henry van de Velde when designing one or more of his textile designs. We are now taking care of
+                        this object by preserving it, yet it has almost never been on display in the museum, which begs the question,
+                        who are we taking care of? And for who are we taking care?
                     </div>
                     <br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
-                        <p style={{fontSize: "15px", fontFamily: "P-ACR-AGON-Regular"}}>SOCIAL</p>. How can technology then aid us in rekindling this potentiality, (re-)invoking this idea of being inspired? Can we create a model that
-                        establishes speculative relationships between the samples in this book on one hand and the digitized
-                        objects in our collection on the other? By means of color extraction, the model tries to evoke a social network,
+                        <p style={{fontSize: "15px", fontFamily: "P-ACR-AGON-Regular"}}>TO TAKE CARE OF. </p>
+                        Taking care of something or someone is a highly social endeavor and is a premise for us to co-exist.
+                        To support this act of caretaking we developed technology of all sorts – this same technology however
+                        can also lead to direct opposite when not used properly. Could we now imagine interfaces of care?
+                        Can we create a model that establishes speculative relationships? For example, to socialize
+                        between the samples in this book on one hand and the digitized objects in our collection on the other?
+                        One such interface could be achieved by means of color extraction, a model that tries to evoke a social network,
                         a system, in which objects relate to one another based on their color palette. At the same time,
                         this reorganization of the collection based on samples made and collected by an unknown colorist,
-                        evokes a "culture of anonymity" while also seeking other approaches to our collection that move beyond
-                        the dominant form of authorship. An inherent quality of the work that often through both its absence and/or presence
-                        determines the visibility or invisibility of an object.
+                        evokes a "culture of anonymity" while also seeking other approaches to our collection that move
+                        beyond the dominant form of authorship. An inherent quality of the work that often determines
+                        the visibility and/or invisibility of an object.
+
                     </div><br/>
                     <div className="left box" style={{fontSize: "15px", fontFamily: "happy-times-NG_regular_master_web"}}>
                         This approach of the collection as a social network, - a speculative system that connects in a transhistorical and transdisciplinary way -
