ac727b8628547585acc4fd21faa44404d9c924c7
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-07 09:25:24 +0200

    redirect to homapage when clicking on the title header.

--- src/components/header+footer/xenoHeader.js
+++ src/components/header+footer/xenoHeader.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const XenoHeader = () => {
     return (
             <div>
-                <div className="scroll-container">
-                    <crypto--h1 className="scroll-text"> models from the past for the future | </crypto--h1>
-                    <crypto--h1 className="scroll-text"> models from the past for the future | </crypto--h1>
-                </div>
+                <Link to="/">
+                    <div className="scroll-container">
+                        <crypto--h1 className="scroll-text"> models from the past for the future | </crypto--h1>
+                        <crypto--h1 className="scroll-text"> models from the past for the future | </crypto--h1>
+                    </div>
+                </Link>
                 <div className="scroll-container ">
                     <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the sensible | </h2>
                     <h2 className="scroll-text"> SPECTRUM | the transhistorical | SPECTRUM | the current | SPECTRUM | the xeno-morphic | SPECTRUM | the past-future | SPECTRUM | the sensible |</h2>
