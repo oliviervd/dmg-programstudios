56e1e8110951da3dd7995c2d52ae625a9f0fe37c
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-07 09:25:42 +0200

    add init sketch.

--- src/components/essay/algoMuseum.js
+++ src/components/essay/algoMuseum.js
@@ -1,5 +1,6 @@
 import React from "react"
 import XenoHeader from "../header+footer/xenoHeader";
+import RedBars from "../sketches/Sketch2_redBars";
 
 const EssayAlgoMuseum = () => {
     return(
@@ -11,6 +12,7 @@ const EssayAlgoMuseum = () => {
                     <div className="button-lang">EN</div>
                 </div>
                 <div>
+                    <RedBars/>
                     <crypto--h1 className="center underlined">THE MUSEUM AND THE COMPUTATIONAL</crypto--h1>
                 </div>
             </div>
