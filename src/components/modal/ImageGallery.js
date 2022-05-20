f03e4efbcfffb5a8bcef512da43bd3ac6d066d2b
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-20 19:04:26 +0200

    add __BRUISES__

--- src/components/modal/ImageGallery.js
+++ src/components/modal/ImageGallery.js
@@ -3,13 +3,6 @@ import React from "react";
 const ImageGallery = ({images}) => {
 
     let l = images.length;
-    console.log(l);
-
-    console.log(images);
-    for (let i = 0; i < l; i++) {
-        console.log(images[i]);
-    }
-
     const imList = images.map(x =>
         <img alt="DREAMING OF IMAGES" className="imageViewer__modal__gallery-image__inner" src={x}></img>
     )
@@ -19,7 +12,6 @@ const ImageGallery = ({images}) => {
         </div>
     )
 
-    console.log(imList);
 }
 
 export default ImageGallery;
