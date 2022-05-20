cd3552cac944724105fd8e17487117070c3221ac
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-20 14:06:14 +0200

    FIX FUCKERY: add Route Based code-splitting __LAZY SUSPENSE__

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // index.js
-import React, {Suspense} from "react";
+import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
@@ -9,21 +9,19 @@ import './svg.css';
 import './glossary.css';
 
 // import pages
-
-const Glossary = React.lazy(() => import("./components/Glossary"))
-const Computational = React.lazy(() => import("./components/models/model2_computational"))
-const Landing = React.lazy(() => import("./components/pages/landing"))
+import Glossary from "./components/Glossary";
+import EssayAlgoMuseum from "./components/essay/algoMuseum";
+import Computational from "./components/models/model2_computational";
+import Landing from "./components/pages/landing";
 
 const rootElement = document.getElementById("root");
 render(
     <BrowserRouter>
         <Routes>
-            <Suspense>
-                <Route path="landing" element={<Landing/>}/>
-                <Route path="/" element={<App />}/>
-                <Route path="glossary" element={<Glossary />}/>
-                <Route path="essay/the-algorithmic-museum" element={<Computational />}/>
-            </Suspense>
+            <Route path="landing" element={<Landing/>}/>
+            <Route path="/" element={<App />}/>
+            <Route path="glossary" element={<Glossary />}/>
+            <Route path="essay/the-algorithmic-museum" element={<Computational />}/>
         </Routes>
     </BrowserRouter>,
     rootElement
