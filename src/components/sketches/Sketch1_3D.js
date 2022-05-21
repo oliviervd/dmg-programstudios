c2327e77652d444aa526b45e00f6422511e99df3
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-21 10:58:11 +0200

    _RESPONSIVE_ Sketch1_3D.js

--- src/components/sketches/Sketch1_3D.js
+++ src/components/sketches/Sketch1_3D.js
@@ -5,8 +5,8 @@ import "./hdc_01.jpeg"
 const Sketch13D = p5 => {
 
     let MAGNITUDE = 10000;
-    let HEIGHT = window.innerHeight
-    let WIDTH = window.innerWidth
+    let _height = window.innerHeight
+    let _width = window.innerWidth
     let GRID_X = 50; //grid amount X
     let GRID_Y = 50;
     let GRID_W;
@@ -20,11 +20,6 @@ const Sketch13D = p5 => {
 
     let canvas;
 
-    let HDC_outer;
-    let HDC_inner;
-    let MODELMUS01;
-    let DOME__OUTER;
-
 
     //colors
     let BG = 0;
@@ -32,17 +27,12 @@ const Sketch13D = p5 => {
     let _green1 = "#00ff67"
     let _green2 = "#afc6ae"
     let _pink1 = "#C6AEAF"
-/*
-    const preload = (p5) => {
-        HDC_inner = p5.loadImage('hdc_01.jpeg');
-        HDC_outer = p5.loadImage('SCMS_PBK_0409.jpeg');
-        MODELMUS01 = p5.loadImage('modelMus00.jpg');
-        p5.redraw();
-    }*/
+    let _bruises
 
 
     const setup = (p5, canvasParantRef) => {
-        canvas = p5.createCanvas(WIDTH, HEIGHT, p5.WEBGL).parent(canvasParantRef);
+        canvas = p5.createCanvas(_width, _height, p5.WEBGL).parent(canvasParantRef);
+        //_bruises = p5.loadImage("49.png");
         //p5.tint(255, 127);
     }
 
@@ -52,11 +42,11 @@ const Sketch13D = p5 => {
         GRID_W = MAGNITUDE / GRID_X
         GRID_H = MAGNITUDE / GRID_Y;
 
-        let eyeX = WIDTH/2.0 + p5.map(p5.mouseX, 0, WIDTH, 0, 1000);
-        let eyeY = HEIGHT/2.0;
-        let eyeZ =(HEIGHT/2.0) / p5.tan(p5.PI*30.0 / 180.0);
-        let centerX = WIDTH/2.0;
-        let centerY = HEIGHT/2.0;
+        let eyeX = _width/2.0 + p5.map(p5.mouseX, 0, _width, 0, 1000);
+        let eyeY = _height/2.0;
+        let eyeZ =(_height/2.0) / p5.tan(p5.PI*30.0 / 180.0);
+        let centerX = _width/2.0;
+        let centerY = _height/2.0;
         let centerZ = 0;
         let upX = 0;
         let upY = 1;
@@ -65,7 +55,7 @@ const Sketch13D = p5 => {
 
         // -------- SCENE -----------//
         p5.background(255);
-        p5.perspective(p5.PI/3.0, p5.float(WIDTH/HEIGHT), 1, 10000);
+        p5.perspective(p5.PI/3.0, p5.float(_width/_height), 1, 10000);
 
         p5.push();
         p5.translate(0, 1000, MOTION); // 750
@@ -88,7 +78,7 @@ const Sketch13D = p5 => {
 
         //IMAGE INSIDE
         p5.push();
-        let _angle = p5.map(p5.mouseY, 0, HEIGHT, 0, 360);
+        let _angle = p5.map(p5.mouseY, 0, _height, 0, 360);
         p5.rotateX(p5.radians(-90));
         p5.rotateY(p5.radians(-30));
         p5.translate(1500, -2200, -1000);
@@ -149,10 +139,10 @@ const Sketch13D = p5 => {
         p5.translate(300, -700, 400);
         p5.stroke(_green2)
         //p5.fill(0, 255, 103, 63)
-        // p5.texture(HDC_inner); // REACTIVATE FOR SPHERE.
+        //p5.texture(_bruises); // REACTIVATE FOR SPHERE.
         p5.rotateX(p5.radians(-90));
         p5.rotateY(p5.radians(p5.frameCount));
-        p5.translate(p5.noise(xOff)*100, p5.noise(yOff)*HEIGHT*0.01, -p5.noise(zOff)*600);
+        p5.translate(p5.noise(xOff)*100, p5.noise(yOff)*_height*0.01, -p5.noise(zOff)*600);
         p5.sphere(300);
         p5.pop()
 
@@ -161,7 +151,7 @@ const Sketch13D = p5 => {
         p5.stroke(_pink1);
         p5.fill(175, 198, 174, 67);
         p5.strokeWeight(2);
-        p5.translate(p5.noise(xOff)*200, p5.noise(yOff)*HEIGHT*0.08, -p5.noise(zOff)*100);
+        p5.translate(p5.noise(xOff)*200, p5.noise(yOff)*_height*0.08, -p5.noise(zOff)*100);
         p5.sphere(500);
 
         p5.pop();
@@ -179,6 +169,12 @@ const Sketch13D = p5 => {
                 console.log("backwards")
             }
         }
+    }
+
+    window.onresize = function() {
+        _width = window.innerWidth;
+        _height = window.innerHeight;
+        canvas.size(_width, _height);
     }
 
     //return (<Sketch preload={preload} setup={setup} draw={draw}/>)
