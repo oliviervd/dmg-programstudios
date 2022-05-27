0526c8bd99d8b92ab2fca52fddca77982d6d4661
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-27 15:41:15 +0200

    __BUILT__ header viz for curatorial statement. MODELS_FROM_THE_PAST_FOR_THE_FUTURE-69

--- src/components/sketches/Sketch_ModelsGrid.js
+++ src/components/sketches/Sketch_ModelsGrid.js
@@ -4,23 +4,40 @@ import Sketch from "react-p5";
 const Sketch_ModelsGrid = p5 => {
 
     let WIDTH = window.innerWidth
-    let HEIGHT = window.innerHeight*(2/3)
+    let HEIGHT = window.innerHeight*(1/4)
 
-    let gridSize = 2;
+    let gridSize = 30;
+    let _happy;
+    let _pacragon;
 
     const setup = (p5, canvasParentRef) => {
         p5.createCanvas(WIDTH, HEIGHT).parent(canvasParentRef);
+        var fontFile_1 = "P-ACR-AGON-Regular.otf"
+        var fontFile_2 = "happy-times-NG_regular_master_web.ttf"
+        _happy = p5.loadFont(fontFile_2, 1000);
+        _pacragon = p5.loadFont(fontFile_1, 1000);
     }
 
     const draw = p5 => {
         p5.background(255);
 
-        let wave = p5.map(p5.sin(p5.radians(p5.frameCount)),-1, 1, 0, 1)
+        let wave = p5.map(p5.sin(p5.radians(p5.frameCount)),-1, 1, 0.5, 1)
+
+        p5.textFont(_pacragon);
+        p5.textSize(WIDTH/10);
+        //p5.stroke(255);
+        p5.fill(255);
+        p5.textAlign(p5.CENTER, p5.CENTER);
+        p5.text("_BREAK", WIDTH/2, HEIGHT/2);
+        //p5.text("_BREAK, MUTATE AND BUILT ON WHAT IS AND ISN'T REAL", WIDTH/2, HEIGHT/2);
+        //p5.text("_BREAK, MUTATE AND BUILT ON WHAT IS AND ISN'T REAL", WIDTH/2, HEIGHT/2);
 
         for (let x = 0; x < gridSize; x++ ){
             for (let y = 0; y < gridSize; y++) {
 
                 let xPos = x * WIDTH/gridSize * wave
+                let xPosRev = x * WIDTH/gridSize/wave
+                //let xPosRev = xPos/2 + WIDTH/gridSize
                 let yPos = y * HEIGHT/gridSize
                 let xWidth = WIDTH/gridSize * wave
                 let xOffSet = (WIDTH/gridSize)/2
@@ -30,6 +47,7 @@ const Sketch_ModelsGrid = p5 => {
                 p5.stroke(0)
                 p5.strokeWeight(5);
                 p5.ellipse( xPos + xOffSet , yPos + yOffSet, WIDTH/gridSize, HEIGHT/gridSize);
+                p5.ellipse( xPosRev + xOffSet , yPos + yOffSet, WIDTH/gridSize, HEIGHT/gridSize);
                 p5.line(xPos, 0, xPos, HEIGHT) // vertical line
             }
         }
