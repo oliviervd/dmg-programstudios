4d3b340f5676684817d736743eed3774e22ae5e9
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-05-29 10:32:08 +0200

    __MUTATE__ clean up code

--- src/components/pages/Glossary.js
+++ src/components/pages/Glossary.js
@@ -1,33 +1,8 @@
-import React, {Suspense} from "react";
-
-const XenoHeader = React.lazy(() => import("./elements/xenoHeader"))
+import React from "react";
 
 const Glossary = () => {
     return(
-        <div>
-            <div className="headerContainer">
-                <Suspense>
-                    <XenoHeader/>
-                </Suspense>
-                <div className="languages_button_box">
-                    <div className="button-lang">NL</div>
-                    <div className="button-lang">EN</div>
-                </div>
-            </div>
-            <div className='glossaryContainer__main'>
-                <div></div>
-                <div>
-                    <crypto--h1>GLOSSARY</crypto--h1>
-                    <h2>XENOMORPHIC</h2>
-                    <p>Meillisoux argues that one can only approach the contingent
-                        "nature of the world, which he calls 'hyperchaos', by creating a toolbox in which constant change could manifest itself.
-                        "As a research platform, we aim to demonstrate the changing nature of museum collections by evoking the fictions, narratives and frameworks
-                        "in which these changes occur. Whether by being part of the exhibition or the lack thereof...</p>
-                </div>
-                <div></div>
-
-            </div>
-        </div>
+        <div></div>
     )
 }
 
