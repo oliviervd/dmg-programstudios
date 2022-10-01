1fcf59dc1e64cd8e5970bed1779e4be966271843
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2022-10-01 11:00:28 +0200

    __MUTATE__ home is where the heart is.

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -11,11 +11,19 @@ const Home = () => {
             <div>
                 <Suspense>
                     <div className="grid--2_6_2">
-                        <div className="grid--3_4_3">
-                            <h2>ABOUT</h2>
-                            <h2>PROJECTS</h2>
-                            <h2>ARCHIVE</h2>
+                        <div className="gridH--even_3">
+                            <div></div>
+                            <div className="grid--3_4_3">
+                                <h2 className="nav--header">ABOUT</h2>
+                                <Link className="nav--header" to="../M01_C01">
+                                    <h2>PROJECTS</h2>
+                                </Link>
+                                <h2 className="nav--header">ARCHIVE</h2>
 
+                            </div>
+                            <div >
+
+                            </div>
                         </div>
                         <XenoHeader header_home={true} header_main={false} header_models={false} header_model={false} header_nav={false}/>
                         <div className="grid--2_6_2">
@@ -36,6 +44,9 @@ const Home = () => {
                         Studio DMG is <u>multimedia platform</u> for collective reflection on the future, past and present of the museum.
                         All studio's reside and co-exist as part of a larger ecosystem. They mingle, converse and change accordingly.
                     </p>
+                </div>
+                <div className="background__transparent__flow">
+
                 </div>
 
                 <h1></h1>
