8633e91875926b1625d6a89d5c014b5072d4ae84
oliviervd <olliviervandhuynslager@gmail.com>
2022-11-12 10:03:21 +0100

    __MUTATE__ : start cleaning.

--- src/components/elements/xenoFooter.js
+++ src/components/elements/xenoFooter.js
@@ -4,11 +4,8 @@ const XenoFooter = () => {
     return(
         <div style={{height:'120px'}}>
             <div className="dotLine"/>
-            <div className="grid--3_4_3">
-                <img src={require("./Logo Design Museum Gent.png")} style={{height:'150px'}}></img>
-                <p>THIS IS A FOOTER</p>
-                <p>A PROJECT BY..</p>
-            </div>
+            <div className="grid--3_4_3"/>
+
         </div>
     )
 }
