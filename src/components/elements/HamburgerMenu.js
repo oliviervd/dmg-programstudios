a1459b1efe6da775bb8f26c6fab4baa940fa69f9
oliviervd <olliviervandhuynslager@gmail.com>
2022-11-20 18:52:44 +0100

    MODELS_FROM_THE_PAST_FOR_THE_FUTURE-99 add links to about and language selector.;

--- src/components/elements/HamburgerMenu.js
+++ src/components/elements/HamburgerMenu.js
@@ -22,21 +22,24 @@ const HamburgerMenu = (props) => {
                 <div>
                     <div onClick={() => setHamburgerOpen(!hamburgerOpen)} className="button-burger"
                          aria-controls="primary-navigation" aria-expanded="false">
-                        <svg className="hamburger">
+                        <svg className="hamburger italic">
                             <rect class="burgerIcon" className="top firstBar" width="60" height="3" x="10" y="25"/>
                             <rect class="burgerIcon" className="top secondBar" width="60" height="3" x="10" y="45"/>
                             <rect class="burgerIcon" className="top" width="60" height="3" x="10" y="65"/>
                         </svg>
                     </div>
                     <div className="hamburgerMenu">
-                        <h1 style={{marginLeft: 20, marginTop: 20, position:"absolute"}} onClick={() => setHamburgerOpen(!hamburgerOpen)}>X</h1>
                         <div>
-                            <h1 onClick={()=>props.setAbout(!props.about)} className="text-center">{headerAbout(props.language)}</h1>
+                            <h1 onClick={()=>props.setAbout(!props.about)} className="text-center italic">{headerAbout(props.language)}</h1>
 
-                            <div className="gridH--even_3">
-                                <h2 className="uppercase text-center" style={{margin: 10}} onClick={() => props.setLanguage("EN")}>EN</h2>
-                                <h2 className="uppercase text-center" style={{margin: 10}} onClick={() => props.setLanguage("NL")}>NL</h2>
-                                <h2 className="uppercase text-center" style={{margin: 10}} onClick={() => props.setLanguage("FR")}>FR</h2>
+                            <div className="grid--even_3">
+                                <div/>
+                                <div className="grid--even_3" style={{marginBottom:0}}>
+                                    <h2 className="uppercase text-center italic" style={{margin: 10}} onClick={() => props.setLanguage("EN")}>EN</h2>
+                                    <h2 className="uppercase text-center italic" style={{margin: 10}} onClick={() => props.setLanguage("NL")}>NL</h2>
+                                    <h2 className="uppercase text-center italic" style={{margin: 10}} onClick={() => props.setLanguage("FR")}>FR</h2>
+                                </div>
+                                <div/>
                             </div>
                         </div>
                     </div>
