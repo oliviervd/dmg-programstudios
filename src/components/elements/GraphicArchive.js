49632c7acf153a19ff344dd5552b7752c310ddac
oliviervd <olliviervandhuynslager@gmail.com>
2022-11-21 19:26:31 +0100

    #MODELS_FROM_THE_PAST_FOR_THE_FUTURE-109 change layout a little.

--- src/components/elements/GraphicArchive.js
+++ src/components/elements/GraphicArchive.js
@@ -31,9 +31,9 @@ const GraphicArchive = (props) => {
                     <div/>
                     <p className='underlined' onClick={()=>setOpenArchive(!openArchive)}>{langHeaderClosed}</p>
                 </div>
-                <div className="grid--even_3 graphicArchiveBox">
-                    <div/>
-                    <div/>
+                <div className="borderLine-left grid--even_3 graphicArchiveBox">
+                    <div className="borderLine-left"/>
+                    <div className="borderLine-left"/>
                     <div>
                         <div className="borderLine-top">
                             <ul className='italic' onClick={()=>props.setVisualIdentity("graphic_archive_01")}>protagonist #1</ul>
