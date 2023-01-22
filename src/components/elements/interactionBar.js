d6ece230b3d578a2a291eb32ba7388dba08cfa41
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-01-22 19:10:02 +0100

    MUTATE - update DB

--- src/components/elements/interactionBar.js
+++ src/components/elements/interactionBar.js
@@ -35,7 +35,7 @@ const InteractionBar = (props) => {
                         <DarkMode darkMode={props.darkMode} setDarkMode={props.setDarkMode}/>
                     }
                     {props.lastFetch &&
-                        <p>last update: 12/03/2022</p>
+                        <p>last update: 22/01/2023</p>
                     }
                     {!props.darkModeShow&&
                         <div/>}
