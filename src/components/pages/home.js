a5debe41fb73151f503400d6af1092b4b0d09f5f
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-01-31 12:55:25 +0100

    MUTATE - update responsive.

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -136,7 +136,7 @@ const Home = () => {
                                                 <div style={{padding: "2vw"}}>
                                                     <div className="grid--even" style={{padding: "2vh"}}>
                                                         <div className="lineH"></div>
-                                                        <h2 className="uppercase">{title_en} style={{fontSize: "40px"}}</h2>
+                                                        <h2 className="uppercase" style={{fontSize: "40px"}}>{title_en} </h2>
                                                     </div>
                                                 </div>
                                             </div>
