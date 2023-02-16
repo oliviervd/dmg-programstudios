789d4981667e5c5326d327685fbf4a3d287757ff
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-02-16 16:26:16 +0100

    MUTATE - init OIKOS. for Studio.

--- src/components/pages/StudioLanding.js
+++ src/components/pages/StudioLanding.js
@@ -1,2 +1,34 @@
-import React
+import React, {Suspense} from "react"
+import {useParams} from "react-router-dom";
 
+const StudioLanding = (props) => {
+
+    const InteractionBar = React.lazy(()=>import("../elements/interactionBar"))
+    const id = useParams(); //fetch id from URL
+
+    return(
+        <div className="full-page container grid-home-main-open">
+                <div>
+                    <h1 className="home ">studio</h1>
+                    <h1 className="home ">{id.id}</h1>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                    <Suspense>
+                        <InteractionBar/>
+                    </Suspense>
+                <div>
+
+                </div>
+        </div>
+
+
+
+    )
+}
+
+export default StudioLanding
