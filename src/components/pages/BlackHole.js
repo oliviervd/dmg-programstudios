f3bcb615309252bad7db30b535bc34c0ea840c0f
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-02-16 16:24:56 +0100

    MUTATE -  blackhole.

--- src/components/pages/BlackHole.js
+++ src/components/pages/BlackHole.js
@@ -5,12 +5,12 @@ function RedirectExample() {
         const timeout = setTimeout(() => {
             // 👇️ redirects to an external URL
             window.location.replace('https://en.wikipedia.org/wiki/Black_hole');
-        }, 5000);
+        }, 3000);
 
         return () => clearTimeout(timeout);
     }, []);
 
-    return <>Will redirect in 5 seconds...</>;
+    return <>Will redirect in 3 seconds...</>;
 }
 
 const BlackHole = () => {
@@ -18,7 +18,7 @@ const BlackHole = () => {
     return (
         <div className="full-page">
             <div style={{position: "absolute", top: "40vh", bottom: "40vh", left: "20vw", right: "20vw"}}>
-                <h1  className="home" style={{fontSize: "5vw"}}>Will redirect in 5 seconds...</h1>
+                <h1  className="home" style={{fontSize: "5vw"}}>Will redirect in 3 seconds...</h1>
             </div>
         </div>
     )
