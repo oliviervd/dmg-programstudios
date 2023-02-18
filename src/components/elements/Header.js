b8101f767b6f87d19e458b9022020446e4fce743
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-02-18 13:42:21 +0100

    _MEND_ remove unused import.

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,5 +1,5 @@
 import React, {lazy, Suspense} from "react";
-import {Link, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {headerAbout, headerTitle, headerTitleBig} from "../utils/data_parsers";
 import {useMediaQuery} from "react-responsive";
 
