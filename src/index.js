1ff820d1217dd28ce42c74cd77ccd557d22a50a0
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-02-18 13:42:40 +0100

    __MUTATE__ reroute.

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ render(
     <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home />}/>
-                <Route path="/color-tagger" element={<ColorTagger />}/>
+                <Route path="/interfaces/color-tagger" element={<ColorTagger />}/>
                 <Route path="/studio/:id" element={<StudioLanding />}/>
                 <Route path="/index" element={<Index />} />
                 <Route path="*" element={<BlackHole />} />
