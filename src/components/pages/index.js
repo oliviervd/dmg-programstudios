92fe3cde779fb534963e3de13298687b317a4b2b
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-02-19 18:00:16 +0100

    __GROW__ add color masonry that matches color selection.

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -27,13 +27,28 @@ const Index = () => {
         setColors(data)
     }
 
+    function fetchImageByColor(color) {
+        const imageList = []
+        for (let i=0; i<colors.length; i++){
+            for (let z=0; z<colors[i]["color_names"].length; z++) {
+                if (colors[i]["color_names"][z].includes(color)) {
+                    //console.log(colors[i]["iiif_image_uris"][z])
+                    imageList.push(colors[i]["iiif_image_uris"][z])
+                }
+            }
+        }
+        return imageList
+    }
+
 
     const HexList = [];
     for (let i=0; i<colors.length; i++){
         // iterate over all colors.
         for (let z=0; z<colors[i]["color_names"].length; z++) {
             for (let hex = 0; hex < colors[i]["color_names"][z].length; hex++) {
-                HexList.push(colors[i]["color_names"][z][hex])
+                if (colors[i]["color_names"][z][hex] != "Gray (X11 gray)"){
+                    HexList.push(colors[i]["color_names"][z][hex])
+                }
             }
         }
     }
@@ -43,7 +58,8 @@ const Index = () => {
     }
     const Hex100 = shuffleFisherYates(_HexCounts) // RANDOMIZE SELECTION OF COLORS USING FISHER YATES
 
-    const Hex100ran = splice(Hex100, 0, 100); // ONLY SELECT FIRST 100 OUT OF SELECTION.
+    console.log(Hex100)
+    const Hex100ran = splice(Hex100, 0, 10000); // ONLY SELECT FIRST 100 OUT OF SELECTION.
 
     // set STYLING (onHover pickup color);
     const [myStyle, setMyStyle] = useState({})
@@ -55,14 +71,24 @@ const Index = () => {
     }
 
     // set Color for selection of collection;
-    const [objectColor, setObjectColor] = useState("");
-    const [showObject, setShowObject] = useState(false);
+    const [objectColor, setObjectColor] = useState("Laurel green");
+    const [showColors, setShowColors] = useState(false);
 
     const HexOptions = Object.entries(Hex100ran).map(([key , i]) =>  (
-        <h2 className="text-center" style={{color:myStyle[`${i}`] ? getKeyByValue(colorRef, key) : "black"}}
-            onClick={()=>setObjectColor(key)} onMouseOver={()=>handleClick(i)} onMouseLeave={()=>handleClick(i)} key={key}>{key} </h2>
+        <p className={"grid-text-autoflow"}
+            //style={{color:myStyle[`${i}`] ? getKeyByValue(colorRef, key) : "black"}}
+            style={{color: "black"}}
+            onClick={()=>setObjectColor(key)} onMouseOver={()=>handleClick(i)}
+            onMouseLeave={()=>handleClick(i)} key={key}>
+            #{key},
+        </p>
     ));
 
+    const images = fetchImageByColor(objectColor)
+    const imageBlock = images.map(image => (
+        <img src={image.replace("/full/0/default.jpg", "/400,/0/default.jpg")}/>
+    ))
+    console.log(images);
 
 
 
@@ -71,18 +97,9 @@ const Index = () => {
             <div className="grid--3_4_3">
                 <h1 className="home">index</h1>
                 <div></div>
-                <h2 className="box-title main" style={{textAlign: "right"}} onClick={()=>navigate("/")}>return.</h2>
+                <h2 className="uppercase text-center" style={{textAlign: "right", margin: 10, marginTop: "-10px"}} onClick={()=>navigate("/")}>home</h2>
             </div>
             <div className="grid--even">
-                <div>
-                    <div className="lineH"/>
-                    <p>systems</p>
-                    <div className="grid--even_10">
-                        <h2>019</h2>
-                        <h2>DESIGN MUSEUM GENT</h2>
-                    </div>
-
-                </div>
                 <div>
                     <div className="lineH"/>
                     <div className="grid--2_6_2">
@@ -90,7 +107,7 @@ const Index = () => {
                         <div></div>
                         <p style={{textAlign:"center"}}>*pseudorandom selection out of {HexList.length} colors observed.</p>
                     </div>
-                    <div className="grid--even_10">
+                    <div className={showColors? "full": "half"}>
                         <Suspense>
                             {HexOptions}
                         </Suspense>
@@ -104,7 +121,14 @@ const Index = () => {
                         <div></div>
                         <div></div>
                     </div>
-                    <h1 style={{color: getKeyByValue(colorRef, objectColor)}}>{objectColor}</h1>
+                    <h2 style={{color: getKeyByValue(colorRef, objectColor)}}>{objectColor}</h2>
+                    <div className={"container-masonry"}>
+                        <div className="masonry">
+                            {imageBlock}
+                        </div>
+                    </div>
+
+
                 </div>
 
                 <div>
@@ -113,6 +137,13 @@ const Index = () => {
                     <div className="grid--even_8">
 
                     </div>
+                </div>
+                <div>
+                    <div className="lineH"/>
+                    <p>systems</p>
+                    <div className="grid--even_10">
+                    </div>
+
                 </div>
             </div>
 
