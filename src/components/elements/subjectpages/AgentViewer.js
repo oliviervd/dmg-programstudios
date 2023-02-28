bb052352c61e7eae8c6183f61bb805414371ec67
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-02-28 12:57:42 +0100

    __MUTATE__ minor change in layout

--- src/components/elements/subjectpages/AgentViewer.js
+++ src/components/elements/subjectpages/AgentViewer.js
@@ -3,18 +3,42 @@ import React from "react"
 const AgentViewer = (props) =>  {
 
     let name = ""
+    let sex = ""
+    let birth = ""
     let _basePERS = props.agent
 
     if (_basePERS) {
         name = _basePERS.LDES_raw.object["https://data.vlaanderen.be/ns/persoon#volledigeNaam"]
+        try {
+            sex = _basePERS.LDES_raw.object["https://data.vlaanderen.be/ns/persoon#geslacht"]["@value"]
+        } catch (error) {}
+
+        try {
+            birth = _basePERS.LDES_raw.object["https://data.vlaanderen.be/ns/persoon#heeftGeboorte"]
+        } catch (error) {}
     }
 
     console.log(name)
     return(
-        <div>
-            {name &&
-                <h1 className={"home"} style={{fontSize: "4vw"}}>{name}</h1>
-            }
+        <div className={"grid--5_95"}>
+            <div></div>
+            <div>
+                <div>
+                    {name &&
+                        <h1 className={"home"} style={{fontSize: "4vw"}}>{name}</h1>
+                    }
+                </div>
+                <div className={"grid--4_6-ObjectViewer"}>
+                    <div></div>
+
+                    {sex != "" &&
+                        <div>
+                            <p className={"underlined"}>gender:</p>
+                            <p>{sex}</p>
+                        </div>
+                    }
+                </div>
+            </div>
         </div>
     )
 }
