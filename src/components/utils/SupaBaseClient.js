2234026a9785061f0fa67e48a7084384d9ed2eaf
oliviervd <olliviervandhuynslager@gmail.com>
2023-04-14 15:22:31 +0200

    fix bug

--- src/components/utils/SupaBaseClient.js
+++ src/components/utils/SupaBaseClient.js
@@ -3,8 +3,8 @@ import invariant from "tiny-invariant";
 
 export function getSupabaseBrowserClient() {
     // get the environment variables
-    //invariant(process.env.REACT_APP_SUPABASE_URL, `Supabase URL was not provided`);
-    //invariant(process.env.REACT_APP_SUPABASE_ANON_KEY, `Supabase Anon key was not provided`);
+    invariant(process.env.REACT_APP_SUPABASE_URL, `Supabase URL was not provided`);
+    invariant(process.env.REACT_APP_SUPABASE_ANON_KEY, `Supabase Anon key was not provided`);
 
     return createClient(
         process.env.REACT_APP_SUPABASE_URL,
