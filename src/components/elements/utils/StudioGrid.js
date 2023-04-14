8527095cdfba3fcd3d2f1b856801a2ccb93a5472
oliviervd <olliviervandhuynslager@gmail.com>
2023-04-14 18:09:17 +0200

    try to fix some shit.

--- src/components/elements/utils/StudioGrid.js
+++ src/components/elements/utils/StudioGrid.js
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import {useQuery} from "@tanstack/react-query";
 import {useNavigate} from "react-router-dom";
 import {fetchDataStudiosPayload, fetchPayloadMediaById} from "../../utils/data_parsers";
+import {wait} from "../../utils/utils";
 import axios from "axios";
 
 const StudioGrid = (props) => {
@@ -34,45 +35,82 @@ const StudioGrid = (props) => {
         })
     })
 
+    if (data) {
+        return(
+            <div style={{paddingLeft: "1vh", paddingRight: "1vh"}}
+                 className={"lineH grid--even_4 HomeProjectGridContainer"}>
+                {data?.data.docs.map((studio => {
+                    let title_en, description, studioImage, studioID, href;
 
-    return(
-        <div style={{paddingLeft: "1vh", paddingRight: "1vh"}}
-             className={"lineH grid--even_4 HomeProjectGridContainer"}>
-            {data?.data.docs.map((studio => {
-                let title_en, description, studioImage, studioID, href;
+                    title_en = fetchDataStudiosPayload(studio, props.language, "title")
+                    description = fetchDataStudiosPayload(studio, props.language, "description")
+                    console.log(description)
+                    studioImage = fetchPayloadMediaById(studio.coverImage.id, media.data.data.docs)
+                    console.log(studioImage)
+                    studioID = ""
 
-                title_en = fetchDataStudiosPayload(studio, props.language, "title")
-                description = fetchDataStudiosPayload(studio, props.language, "description")
-                console.log(description)
-                studioImage = fetchPayloadMediaById(studio.coverImage.id, media.data.data.docs)
-                console.log(studioImage)
-                studioID = ""
+                    href = ""
+                    //href = "/studio/" + studio.title_en.split(" ")[1].toLowerCase();
 
-                href = ""
-                //href = "/studio/" + studio.title_en.split(" ")[1].toLowerCase();
-
-                const routeChange = () => {
-                    navigate(href);
-                }
-                return (
-                    <div id="HomeProjectGrid" className="rowScroll fade-in open">
-                        <div>
-                            <h2 className="text-center uppercase box-title grow main"
-                                onClick={routeChange}>{title_en}</h2>
-                            <p className="uppercase justify padding-10"
-                               style={{height: '10vh'}}>{description}</p>
-                            <img className="img__fit center" alt={""} src={studioImage}
-                                //onClick={() => props.setCarouselState(!props.carouselState)}
-                            />
+                    const routeChange = () => {
+                        navigate(href);
+                    }
+                    return (
+                        <div id="HomeProjectGrid" className="rowScroll fade-in open">
+                            <div>
+                                <h2 className="text-center uppercase box-title grow main"
+                                    onClick={routeChange}>{title_en}</h2>
+                                <p className="uppercase justify padding-10"
+                                   style={{height: '10vh'}}>{description}</p>
+                                <img className="img__fit center" alt={""} src={studioImage}
+                                    //onClick={() => props.setCarouselState(!props.carouselState)}
+                                />
+                            </div>
                         </div>
-                    </div>
-                )
+                    )
 
-            }))}
-        </div>
-    )
+                }))}
+            </div>
+        )
+    } else {
+        wait(100)
+        return(
+            <div style={{paddingLeft: "1vh", paddingRight: "1vh"}}
+                 className={"lineH grid--even_4 HomeProjectGridContainer"}>
+                {data?.data.docs.map((studio => {
+                    let title_en, description, studioImage, studioID, href;
 
+                    title_en = fetchDataStudiosPayload(studio, props.language, "title")
+                    description = fetchDataStudiosPayload(studio, props.language, "description")
+                    console.log(description)
+                    studioImage = fetchPayloadMediaById(studio.coverImage.id, media.data.data.docs)
+                    console.log(studioImage)
+                    studioID = ""
 
+                    href = ""
+                    //href = "/studio/" + studio.title_en.split(" ")[1].toLowerCase();
+
+                    const routeChange = () => {
+                        navigate(href);
+                    }
+                    return (
+                        <div id="HomeProjectGrid" className="rowScroll fade-in open">
+                            <div>
+                                <h2 className="text-center uppercase box-title grow main"
+                                    onClick={routeChange}>{title_en}</h2>
+                                <p className="uppercase justify padding-10"
+                                   style={{height: '10vh'}}>{description}</p>
+                                <img className="img__fit center" alt={""} src={studioImage}
+                                    //onClick={() => props.setCarouselState(!props.carouselState)}
+                                />
+                            </div>
+                        </div>
+                    )
+
+                }))}
+            </div>
+        )
+    }
 }
 
 export default StudioGrid
