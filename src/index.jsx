be60fb2f16f08abc70ace58a8bda8242639e0c6b
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-04-28 16:01:29 +0200

    __MUTATE__ change path

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-// index.jsx
+// index.tsx
 import { render } from "react-dom";
 import { BrowserRouter, Route, Routes} from "react-router-dom";
 import {QueryClient, QueryClientProvider, } from "@tanstack/react-query";
@@ -14,7 +14,7 @@ import './styles/layouts.css'
 // import pages
 import Home from "./components/pages/home";
 import StudioLanding from "./components/pages/StudioLanding"
-import Index from "./components/pages/index"
+import Index from "./components/pages"
 import ObjectPage from "./components/pages/ObjectPage";
 import AgentPage from "./components/pages/AgentPage";
 import {Analytics} from "@vercel/analytics/react";
@@ -36,8 +36,7 @@ render(
                     <Routes>
                         <Route path="/" element={<Home />}/>
                         <Route path="/studio/:id" element={<StudioLanding />}/>
-                        <Route path="/index/:type" element={<Index />}>
-                        </Route>
+                        <Route path="/index/:type" element={<Index />}/>
                         <Route path="/index/object/:id" element={<ObjectPage />}></Route>
                         <Route path="/index/agent/:id" element={<AgentPage/>}></Route>
                     </Routes>
