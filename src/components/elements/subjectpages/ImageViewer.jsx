1839d7b52e0a0b27de5539b2abd52b3c721820d3
oliviervd <olliviervandhuynslager@gmail.com>
2023-05-17 08:32:12 +0200

    __GROW__ add licence (CC) and attribution information to imageviewer. #112: close. close #112

--- src/components/elements/subjectpages/ImageViewer.jsx
+++ src/components/elements/subjectpages/ImageViewer.jsx
@@ -1,12 +1,18 @@
-import React from "react"
+import React, {useState} from "react"
+import {at} from "es5-ext/string/#";
 
 const ImageViewer = (props) => {
 
+    const [showImageInfo, setShowImageInfo] = useState(false);
     let attribution, license;
+
 
     try {
         attribution = props.details.attributions[0]
+        attribution = attribution.split(",")
     } catch(e) {attribution = "unknown"}
+
+    console.log(attribution)
 
     try {
         license = props.details.CC_Licenses[0]
@@ -15,7 +21,15 @@ const ImageViewer = (props) => {
     return(
         <div>
             <div className={"imageContainer"}>
-                <p className={"infoIcon"}>i</p>
+                <span className={"infoIcon"} onClick={()=>setShowImageInfo(!showImageInfo)}>i</span>
+                {showImageInfo &&
+                    <div className={"hiddenInformation"}>
+                        {attribution.map((item)=>{
+                            return <p>{item}</p>
+                        })}
+                        <a href={license} target={"_blank"}>{license}</a>
+                    </div>
+                }
                 <img alt="loading.." className="img__fit" style={{paddingLeft: "5%"}} src={props.media.replace("/full/0/default.jpg", "/1000,/0/default.jpg")}/>
             </div>
         </div>
