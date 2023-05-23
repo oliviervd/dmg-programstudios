14c6d8b0fa0c5d5e87580ae0b7473b7c01a12101
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-05-23 19:49:29 +0200

    __GROW__ #187 - add CC_License information to client.

--- src/components/utils/SupabaseQueries.js
+++ src/components/utils/SupabaseQueries.js
@@ -3,19 +3,19 @@ export function getObjects(client) {
         .from("dmg_objects_LDES")
         .select("color_names, HEX_values, iiif_image_uris, CC_Licenses, attributions, objectNumber, LDES_raw",  {'head':false})
         .not("color_names", 'is', null)
-    return Promise.resolve([])
+    //return Promise.resolve([])
 }
 
 export function getThesaurus(client) {
     return client
         .from("dmg_thesaurus_LDES")
         .select("*",  {'head':false})
-    return Promise.resolve([])
+    //return Promise.resolve([])
 }
 
 export function getAgents(client) {
     return client
         .from("dmg_personen_LDES")
         .select("*",  {'head':false})
-    return Promise.resolve([])
+    //return Promise.resolve([])
 }
